720e731277979e5445447047dd30e2d6d2285b86
ravjohal <ravneetjohal@gmail.com>
2014-05-27 00:24:16 -0700

    routing from signin to dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,8 @@ class ApplicationController < ActionController::Base
     redirect_to (request.referrer || root_path)
   end
 
+  def after_sign_in_path_for(resource)
+    user_root_path(resource)
+  end
+
 end

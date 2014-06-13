207ddd4634842e858cd1a3bb4c80a054118bae84
ravjohal <ravneetjohal@gmail.com>
2014-06-13 00:43:57 -0700

    added fields to cases/new

--- app/models/case.rb
+++ app/models/case.rb
@@ -2,4 +2,8 @@ class Case < ActiveRecord::Base
 	belongs_to :case_type
 	belongs_to :subtype
 	belongs_to :attorney
+
+	validates :number, :presence => true
+	validates :case_type, :presence => true
+	validates :subtype, :presence => true
 end

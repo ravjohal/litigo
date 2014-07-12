897a07c860c50ea696a8754cb7f2fd02feffa5f4
ravjohal <ravneetjohal@gmail.com>
2014-07-12 13:44:02 -0700

    added first part of modal, which doesn't quite work yet

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   root :to => "visitors#index"
   get '/dashboard/:id' => 'users#show', as: :user_root # TODO: Change the page where this is routed to
+  get "case/new_modal" => 'case#new_modal', :as => :new_modal
+
   devise_for :users
   resources :users
   resources :cases

45738cbd391b613a25558ae40b0fee36180bde9f
ravjohal <ravneetjohal@gmail.com>
2014-07-12 12:39:07 -0700

    changed syntax and added bootstrap-sprockets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap/sprockets
 //= require jquery_ujs
 //= require jquery.metisMenu.js
 //= require turbolinks
-//= require bootstrap
-//= require bootstrap-modal
+//= require bootstrap/modal
 //= require_tree .

32ff92f3753384095529dd408ea4153bc7910e28
Russ Savage <russellsavage@gmail.com>
2014-07-12 18:21:37 -0700

    Updated logic for sub menu
    removed metismenu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require jquery.metisMenu.js
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module Litigo
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config.assets.paths << "#{Rails.root}/app/assets/fonts"
-    config.assets.precompile += ["jquery.metisMenu.js"]
   end
 end

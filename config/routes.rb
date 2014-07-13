3be2e7f4d3f250d5a85bca56a2c51d17ea3f340c
ravjohal <ravneetjohal@gmail.com>
2014-07-12 19:56:05 -0700

    added modal for adding contacts

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,7 @@ Rails.application.routes.draw do
 
   root :to => "visitors#index"
   get '/dashboard/:id' => 'users#show', as: :user_root # TODO: Change the page where this is routed to
-  get "/cases/new_case" => 'cases#new_case', :as => :new_modal
-
+  
   devise_for :users
   resources :users
   resources :cases

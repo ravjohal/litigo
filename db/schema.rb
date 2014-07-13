c1028c4626f41f4df9deca3367a7e5875ee3bb91
ravjohal <ravneetjohal@gmail.com>
2014-07-13 16:43:14 -0700

    added modal footers

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20140713225935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "attorneys", force: true do |t|
     t.string   "attorney_type"

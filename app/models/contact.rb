c88a9238e63a8a2f1583dc06f1692812ce2ce4af
rogergraves <rogergraves@gmail.com>
2014-07-13 14:43:40 -0700

    Add length validations for phone and fax numbers on contact

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,7 @@ class Contact < ActiveRecord::Base
 
 	validates :contactable_type, :presence => true
 	validates_presence_of :first_name, :last_name, :contactable_type
+  validates :phone_number, length: { maximum: 10 }
+  validates :fax_number, length: { maximum: 10 }
+
 end

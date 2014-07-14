f6ad765b85e567953559ae4385fa1eef53a8306c
Russ Savage <russellsavage@gmail.com>
2014-07-13 17:55:28 -0700

    adding google_email to the users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714001757) do
+ActiveRecord::Schema.define(version: 20140714005237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,7 @@ ActiveRecord::Schema.define(version: 20140714001757) do
     t.string   "oauth_refresh_token"
     t.string   "oauth_token"
     t.datetime "oauth_expires_at"
+    t.string   "google_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

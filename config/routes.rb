61de3ab5b8e750630660e577c35f42c477cd4b6c
ravjohal <ravneetjohal@gmail.com>
2014-08-04 19:02:55 -0400

    fixed issue: litigo logo link now takes user to cases page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
   resources :attorneys
   resources :witnesses
 
+  authenticated :user do
+    root :to => "cases#index", as: :authenticated_root
+  end
+
   root :to => "visitors#index"
   get '/dashboard/:id' => 'users#show', as: :user_root # TODO: Change the page where this is routed to
   get '/auth/google_oauth2/callback' => 'users#save_google_oauth'

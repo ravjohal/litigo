da19d391228957297d2112ffcb31b7025cae4dd0
ravjohal <ravneetjohal@gmail.com>
2014-08-15 20:24:02 -0700

    added sorting and notes modal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140816024648) do
+ActiveRecord::Schema.define(version: 20140816031725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -128,6 +128,7 @@ ActiveRecord::Schema.define(version: 20140816024648) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "case_id"
+    t.integer  "user_id"
   end
 
   create_table "plantiffs", force: true do |t|

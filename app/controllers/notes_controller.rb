e90bfd887dfc50cead566fadc7c330d8397e7b58
ravjohal <ravneetjohal@gmail.com>
2014-08-15 19:48:14 -0700

    added notes scaffold and associations

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,81 @@
+class NotesController < ApplicationController
+  before_filter :authenticate_user!
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+  # GET /notes
+  # GET /notes.json
+  def index
+    @user = current_user
+    @notes = Note.all
+  end
+
+  # GET /notes/1
+  # GET /notes/1.json
+  def show
+    @user = current_user
+  end
+
+  # GET /notes/new
+  def new
+    @note = Note.new
+  end
+
+  # GET /notes/1/edit
+  def edit
+    @user = current_user
+  end
+
+  # POST /notes
+  # POST /notes.json
+  def create
+    @user = current_user
+    @note = Note.new(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to @note, notice: 'Note was successfully created.' }
+        format.json { render :show, status: :created, location: @note }
+      else
+        format.html { render :new }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /notes/1
+  # PATCH/PUT /notes/1.json
+  def update
+    @user = current_user
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html { redirect_to @note, notice: 'Note was successfully updated.' }
+        format.json { render :show, status: :ok, location: @note }
+      else
+        format.html { render :edit }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /notes/1
+  # DELETE /notes/1.json
+  def destroy
+    @user = current_user
+    @note.destroy
+    respond_to do |format|
+      format.html { redirect_to notes_url, notice: 'Note was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_note
+      @note = Note.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def note_params
+      params.require(:note).permit(:note)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  resources :notes
+
   resources :documents
 
   resources :events

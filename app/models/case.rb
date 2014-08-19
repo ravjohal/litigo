c28f908640f107bd8385a1c867c38974edda2e90
ravjohal <ravneetjohal@gmail.com>
2014-08-19 03:20:12 -0700

    tweaked the case model a bit more #{76664502}

--- app/models/case.rb
+++ app/models/case.rb
@@ -1,11 +1,12 @@
 class Case < ActiveRecord::Base
 
-	#has_many :assigned_attorneys, :through => :contacts, source: :contactable, source_type: 'Attorney'
-	#has_many :assigned_clients, :through => :contacts, source: :contactable, source_type: 'Client'
-  #has_many :assigned_defendants, :through => :contacts, source: :contactable, source_type: 'Defendant'
-  #has_many :assigned_plantiffs, :through => :contacts, source: :contactable, source_type: 'Plantiff'
-  #has_many :assigned_staffs, :through => :contacts, source: :contactable, source_type: 'Staff'
   has_many :contacts
+	has_many :assigned_attorneys, :through => :contacts, source: :contactable, source_type: 'Attorney'
+	has_many :assigned_clients, :through => :contacts, source: :contactable, source_type: 'Client'
+  has_many :assigned_defendants, :through => :contacts, source: :contactable, source_type: 'Defendant'
+  has_many :assigned_plantiffs, :through => :contacts, source: :contactable, source_type: 'Plantiff'
+  has_many :assigned_staffs, :through => :contacts, source: :contactable, source_type: 'Staff'
+  
   #has_many :attorneys, :through => :contacts
   #has_many :clients, :through => :contacts
   #has_many :defendants, :through => :contacts

e248be5388778bcebe22ad21ae3b9273fd9f745b
ravjohal <ravneetjohal@gmail.com>
2014-08-25 00:22:11 -0700

    removed the apartment gem error

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -1,48 +1,48 @@
-# Require whichever elevator you're using below here...
-#
- require 'apartment/elevators/generic'
-# require 'apartment/elevators/domain'
-#require 'apartment/elevators/subdomain'
+# # Require whichever elevator you're using below here...
+# #
+#  require 'apartment/elevators/generic'
+# # require 'apartment/elevators/domain'
+# #require 'apartment/elevators/subdomain'
 
-#
-# Apartment Configuration
-#
-Apartment.configure do |config|
+# #
+# # Apartment Configuration
+# #
+# Apartment.configure do |config|
 
-  # These models will not be multi-tenanted,
-  # but remain in the global (public) namespace
-  #
-  # An example might be a Customer or Tenant model that stores each tenant information
-  # ex:
-  #
-  # config.excluded_models = %w{Tenant}
-  #
-  config.excluded_models = %w{Firm}
+#   # These models will not be multi-tenanted,
+#   # but remain in the global (public) namespace
+#   #
+#   # An example might be a Customer or Tenant model that stores each tenant information
+#   # ex:
+#   #
+#   # config.excluded_models = %w{Tenant}
+#   #
+#   config.excluded_models = %w{Firm}
 
-  # use postgres schemas?
-  config.use_schemas = true
+#   # use postgres schemas?
+#   config.use_schemas = true
 
-  # use raw SQL dumps for creating postgres schemas? (only appies with use_schemas set to true)
-  #config.use_sql = true
+#   # use raw SQL dumps for creating postgres schemas? (only appies with use_schemas set to true)
+#   #config.use_sql = true
 
-  # configure persistent schemas (E.g. hstore )
-  # config.persistent_schemas = %w{ hstore }
+#   # configure persistent schemas (E.g. hstore )
+#   # config.persistent_schemas = %w{ hstore }
 
-  # add the Rails environment to database names?
-  # config.prepend_environment = true
-  # config.append_environment = true
+#   # add the Rails environment to database names?
+#   # config.prepend_environment = true
+#   # config.append_environment = true
 
-  # supply list of database names for migrations to run on
-  config.tenant_names = lambda{ Firm.pluck :name }
-end
+#   # supply list of database names for migrations to run on
+#   config.tenant_names = lambda{ Firm.pluck :name }
+# end
 
-##
-# Elevator Configuration
+# ##
+# # Elevator Configuration
 
- Rails.application.config.middleware.use 'Apartment::Elevators::Generic', lambda { |request|
-    request.host.reverse
- }
+#  Rails.application.config.middleware.use 'Apartment::Elevators::Generic', lambda { |request|
+#     request.host.reverse
+#  }
 
-# Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
+# # Rails.application.config.middleware.use 'Apartment::Elevators::Domain'
 
-# Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'
+# # Rails.application.config.middleware.use 'Apartment::Elevators::Subdomain'

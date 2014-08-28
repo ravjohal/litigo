b38fa35d10b1b52a17680ee34be0e97382f0993e
bensmalley <bensmalley@bensmalley-3.local>
2014-08-28 01:28:53 -0700

    Merge branch 'master' of https://github.com/ravjohal/litigo
    Conflicts:
    app/views/layouts/_side_navigation.html.erb

--- app/models/case.rb
+++ app/models/case.rb
@@ -16,6 +16,7 @@ class Case < ActiveRecord::Base
   has_one :incident, dependent: :destroy
 
   belongs_to :user
+  belongs_to :firm
   has_and_belongs_to_many :documents
   has_and_belongs_to_many :tasks
   has_and_belongs_to_many :events

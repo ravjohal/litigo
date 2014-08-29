59a7864e3dcfb19a5d07389ed30cb21e54f9bcc7
ravjohal <ravneetjohal@gmail.com>
2014-08-29 03:22:12 -0700

    tenant switch upon signin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
     unless current_user.admin?
       unless @user == current_user
         redirect_to :back, :alert => "Access denied."
       end
     end
+
+    Apartment::Tenant.switch(@user.firm)
   end
 
   def update

bb123a8414f965fed247f1d07aaad3c8dd518efe
ravjohal <ravneetjohal@gmail.com>
2014-08-29 00:54:19 -0700

    firm associations not needed anymore as we are using apartment

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -1,12 +1,3 @@
 class Firm < ActiveRecord::Base
-	has_many :cases
-	has_many :users
-	has_many :contacts
-	has_many :assigned_attorneys, :through => :contacts, source: :contactable, source_type: 'Attorney'
-	has_many :assigned_clients, :through => :contacts, source: :contactable, source_type: 'Client'
-    has_many :assigned_defendants, :through => :contacts, source: :contactable, source_type: 'Defendant'
-    has_many :assigned_plantiffs, :through => :contacts, source: :contactable, source_type: 'Plantiff'
-    has_many :assigned_staffs, :through => :contacts, source: :contactable, source_type: 'Staff'
-    has_many :assigned_witnesses, :through => :contacts, source: :contactable, source_type: 'Witness'
-	has_many :documents
+	
 end

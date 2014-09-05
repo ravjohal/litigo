2f73d8614205090589809136ff79396ad64b259e
bensmalley <bensmalley@bensmalley-3.local>
2014-09-05 15:34:23 -0700

    New user Tour

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,3 +2,4 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.assets.precompile += %w( homepage.css)
+Rails.application.config.assets.precompile += %w( hopscotch.js)

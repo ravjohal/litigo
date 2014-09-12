4856a2ca38ad0b92be36147d21f9648bde23e7ee
ravjohal <ravneetjohal@gmail.com>
2014-09-12 04:07:29 -0700

    added incident and medical creation [#77781520] [#77998362]

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,15 +1,75 @@
 class IncidentsController < ApplicationController
   before_filter :authenticate_user!
   before_action :get_case
+  before_action :set_user
 
   def show
-    @incident = Incident.find(params[:id])
+    @incident = @case.incident
+  end
+
+  def new
+    @incident = @case.build_incident
+  end
+
+  # GET case/1/incidents/1/edit
+  def edit
+  	# @case = get_case
+  	@incident = @case.incident
+  end
+
+  def create
+  	@incident = @case.build_incident(incident_params)
+
+    respond_to do |format|
+      if @incident.save
+        format.html { redirect_to [@case, @incident], notice: 'incident was successfully created.' }
+        format.json { render :show, status: :created, location: [@case, @incident] }
+      else
+        format.html { render :new }
+        format.json { render json: @incident.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /incidents/1
+  # PATCH/PUT /incidents/1.json
+  def update
+  	@incident = @case.incident
+
+    respond_to do |format|
+      if @incident.update(incident_params)
+        format.html { redirect_to [@case, @incident], notice: 'incident was successfully updated.' }
+        format.json { render :show, status: :ok, location: @incident }
+      else
+        format.html { render :edit }
+        format.json { render json: @incident.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /incidents/1
+  # DELETE /incidents/1.json
+  def destroy
+    @incident.destroy
+    respond_to do |format|
+      format.html { redirect_to incidents_url, notice: 'incident was successfully destroyed.' }
+      format.json { head :no_content }
+    end
   end
 
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_incident
+      @incident = @case.incident.find(params[:id])
+    end
 
-  def get_case
-    @case = Case.find(params[:case_id])
-  end
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def incident_params
+      params.require(:incident).permit(:incident_date, :statute_of_limitations, :defendant_liability, :alcohol_involved, :weather_factor, :property_damage, :airbag_deployed, :speed, :police_report, :case_id)
+    end
+
+  	def get_case
+   	 @case = Case.find(params[:case_id])
+  	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140911072234) do
+ActiveRecord::Schema.define(version: 20140912105832) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -184,6 +184,7 @@ ActiveRecord::Schema.define(version: 20140911072234) do
     t.string   "treatment_type"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "case_id"
   end
 
   create_table "notes", force: true do |t|

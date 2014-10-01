228343021ea09b397c2093561dd72a295c5a46f5
Jozef <jozefw@sbcglobal.net>
2014-09-30 17:18:19 -0700

    added line 18 enable extension hstore

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 20140923071425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
 
   create_table "case_documents", force: true do |t|
     t.integer  "case_id"

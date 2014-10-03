51fc8ddeff73e0adadd9e6ac835753f1c89bf7f5
ravjohal <ravneetjohal@gmail.com>
2014-10-02 23:40:59 -0700

    [#79989004]

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,4 +5,15 @@ class Document < ActiveRecord::Base
 	has_many :cases, :through => :case_documents
 
 	mount_uploader :document, DocumentUploader
+
+	before_destroy :clean_s3
+
+	private
+	  def clean_s3
+	    document.remove!
+	    #document.thumb.remove! # if you have thumb version or any other version
+	  rescue Excon::Errors::Error => error
+	    puts "Something gone wrong"
+	    false
+	  end
 end

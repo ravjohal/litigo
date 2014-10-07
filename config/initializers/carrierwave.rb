618d92f3ff3ff9ad7594e9640fa876df1e1cc968
ravjohal <ravneetjohal@gmail.com>
2014-10-06 22:26:42 -0700

    removed exposed keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,9 +12,9 @@ CarrierWave.configure do |config|
  
     # Configuration for Amazon S3
     :provider              => 'AWS',
-    :aws_access_key_id     => ENV['S3_KEY'],
-    :aws_secret_access_key => ENV['S3_SECRET'] #,
-    #:region                => ENV['S3_REGION']
+    :aws_access_key_id     => S3_KEY,
+    :aws_secret_access_key => S3_SECRET #,
+    #:region                => S3_REGION
   }
  
   # # For testing, upload files to local `tmp` folder.
@@ -28,7 +28,7 @@ CarrierWave.configure do |config|
  
    config.cache_dir = "#{Rails.root}/tmp/uploads"                  # To let CarrierWave work on heroku
  
-   config.fog_directory    = ENV['S3_BUCKET_NAME']
+   config.fog_directory    = S3_BUCKET_NAME
   # config.s3_access_policy = :public_read                          # Generate http:// urls. Defaults to :authenticated_read (https://)
   # config.fog_host         = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"
 end

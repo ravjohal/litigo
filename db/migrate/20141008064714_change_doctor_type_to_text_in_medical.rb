b623745d49a8594695de74aa39ef6e7220b42866
ravjohal <ravneetjohal@gmail.com>
2014-10-10 19:55:59 -0700

    [#80095476], [#80095456]

--- db/migrate/20141008064714_change_doctor_type_to_text_in_medical.rb
+++ db/migrate/20141008064714_change_doctor_type_to_text_in_medical.rb
@@ -1,9 +1,9 @@
 class ChangeDoctorTypeToTextInMedical < ActiveRecord::Migration
   def self.up
-    change_column :medicals, :doctor_type, :text
+    change_column :medicals, :doctor_type, :hstore
   end
  
   def self.down
-    change_column :medicals, :doctor_type, :string
+    change_column :medicals, :doctor_type, :text
   end
 end

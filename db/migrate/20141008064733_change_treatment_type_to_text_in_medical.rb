d268581ac997e56bbfb5f95239758192afaaee26
ravjohal <ravneetjohal@gmail.com>
2014-10-10 19:59:15 -0700

    fixed hstore bug

--- db/migrate/20141008064733_change_treatment_type_to_text_in_medical.rb
+++ db/migrate/20141008064733_change_treatment_type_to_text_in_medical.rb
@@ -1,10 +1,10 @@
 class ChangeTreatmentTypeToTextInMedical < ActiveRecord::Migration
   def self.up
-    change_column :medicals, :treatment_type, :hstore
+    change_column :medicals, :treatment_type, :text
   end
  
   def self.down
-    change_column :medicals, :treatment_type, :text
+    change_column :medicals, :treatment_type, :string
   end
 
 end

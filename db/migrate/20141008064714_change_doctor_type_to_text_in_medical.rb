70bc442a233e2f6471799d4390715672c4bd2af5
ravjohal <ravneetjohal@gmail.com>
2014-10-10 19:49:07 -0700

    rolled back and fixed a few migrations

--- db/migrate/20141008064714_change_doctor_type_to_text_in_medical.rb
+++ db/migrate/20141008064714_change_doctor_type_to_text_in_medical.rb
@@ -1,5 +1,9 @@
 class ChangeDoctorTypeToTextInMedical < ActiveRecord::Migration
-  def change
-  	change_column :medicals, :doctor_type, :text
+  def self.up
+    change_column :medicals, :doctor_type, :text
+  end
+ 
+  def self.down
+    change_column :medicals, :doctor_type, :string
   end
 end
--- db/migrate/20141008064733_change_treatment_type_to_text_in_medical.rb
+++ db/migrate/20141008064733_change_treatment_type_to_text_in_medical.rb
@@ -1,5 +1,10 @@
 class ChangeTreatmentTypeToTextInMedical < ActiveRecord::Migration
-  def change
+  def self.up
     change_column :medicals, :treatment_type, :text
   end
+ 
+  def self.down
+    change_column :medicals, :treatment_type, :string
+  end
+
 end

aa47c9763354f557125f7573c5110863fbecdf02
ravjohal <ravneetjohal@gmail.com>
2014-10-10 20:01:04 -0700

    fixed another hstore bug

--- db/migrate/20141008064714_change_doctor_type_to_text_in_medical.rb
+++ db/migrate/20141008064714_change_doctor_type_to_text_in_medical.rb
@@ -1,9 +1,9 @@
 class ChangeDoctorTypeToTextInMedical < ActiveRecord::Migration
   def self.up
-    change_column :medicals, :doctor_type, :hstore
+    change_column :medicals, :doctor_type, :text
   end
  
   def self.down
-    change_column :medicals, :doctor_type, :text
+    change_column :medicals, :doctor_type, :string
   end
 end

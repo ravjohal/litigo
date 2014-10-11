8f57c154102c4a04be4a1b0b2fc29e9009abbe37
ravjohal <ravneetjohal@gmail.com>
2014-10-10 19:55:59 -0700

    [#80095476], [#80095456]

--- db/migrate/20141008064733_change_treatment_type_to_text_in_medical.rb
+++ db/migrate/20141008064733_change_treatment_type_to_text_in_medical.rb
@@ -1,10 +1,10 @@
 class ChangeTreatmentTypeToTextInMedical < ActiveRecord::Migration
   def self.up
-    change_column :medicals, :treatment_type, :text
+    change_column :medicals, :treatment_type, :hstore
   end
  
   def self.down
-    change_column :medicals, :treatment_type, :string
+    change_column :medicals, :treatment_type, :text
   end
 
 end

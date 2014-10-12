da6ad1e41916653013edbd52a3345d21b8fded09
bensmalley <bensmalley@bensmalley-3.local>
2014-10-12 14:19:51 -0700

    Add glyph to dash config

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141008075402) do
+ActiveRecord::Schema.define(version: 20141008075403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -234,10 +234,6 @@ ActiveRecord::Schema.define(version: 20141008075402) do
     t.boolean  "stitches"
     t.boolean  "future_surgery"
     t.decimal  "future_medicals"
-    t.integer  "firm_id"
-    t.integer  "medical_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
   end
 
   create_table "medicals", force: true do |t|

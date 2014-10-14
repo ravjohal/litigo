b200838905ace91cda94931c1f18f3a4556ee50a
ravjohal <ravneetjohal@gmail.com>
2014-10-13 22:51:01 -0700

    fixed some issues with styling

--- app/controllers/medicals_controller.rb
+++ app/controllers/medicals_controller.rb
@@ -48,6 +48,21 @@ class MedicalsController < ApplicationController
   def update
     @medical = @case.medical
 
+    #puts "DOCTOR TYPE: " + params[:data].to_s
+    # if params[:data][:doctor_type]
+    #   params[:data][:doctor_type].each do |doc|
+    #     doctor_type << doc
+    #   end
+    #   @medical.data << {:doctor_type => doctor_type}
+    # end
+    # if params[:treatment_type]
+    #   params[:treatment_type].each do |treat|
+    #     treatment_type << treat
+    #   end
+    #   @medical.data << {:treatment_type => treatment_type}
+    # end
+    @medical.data_will_change!
+
     respond_to do |format|
       if @medical.update(medical_params)
         format.html { redirect_to [@case, @medical], notice: 'Medical was successfully updated.' }
@@ -77,6 +92,15 @@ class MedicalsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def medical_params
-      params.require(:medical).permit(:total_med_bills, :subrogated_amount, :injuries_within_three_days, :length_of_treatment, :length_of_treatment, :doctor_type, :treatment_type, :injuries_attributes => [:injury_type, :region, :code, :created_at, :updated_at, :id])
+      params.require(:medical).permit(:total_med_bills, :subrogated_amount, :injuries_within_three_days, :length_of_treatment, 
+                                  :length_of_treatment_unit, :injury_summary, :medical_summary, 
+                                  :earnings_lost, :treatment_gap, :injections, :hospitalization, :hospital_stay_length, 
+                                  :hospital_stay_length_unit, :data => [:doctor_type, :treatment_type], :injuries_attributes => [:injury_type, :region, :code, :dominant_side, :joint_fracture,
+                                  :displaced_fracture, :disfigurement, :impairment, :permanence, :disabled,
+                                  :disabled_percent, :surgery, :surgery_count, :surgery_type, :casted_fracture,
+                                  :stitches, :future_surgery, :future_medicals, :id]).tap do |whitelisted|
+                                    whitelisted[:data] = params[:medical][:data]
+                                  end
+
     end
 end

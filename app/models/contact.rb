5e13f03f707cafc336c90d5c36a6df9269304b9e
ravjohal <ravneetjohal@gmail.com>
2014-10-16 23:56:50 -0700

    [#80070544]

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ActiveRecord::Base
 	
-  TYPES = ['Attorney', 'Client', 'Witness', 'Plaintiff', 'Defendant', 'Staff', 'General']
+  TYPES = ['Attorney', 'Client', 'Plaintiff', 'Defendant', 'Staff', 'Judge', 'Witness', 'General']
 
   belongs_to :case
 	belongs_to :user
---
+++ app/models/judge.rb
@@ -0,0 +1,2 @@
+class Judge < Contact
+end

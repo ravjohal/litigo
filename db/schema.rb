791f63847531c0597d868c7a300b9906ba3bab26
Jozef <jozefw@sbcglobal.net>
2014-10-17 22:37:24 -0700

    added total hours to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018051107) do
+ActiveRecord::Schema.define(version: 20141018053108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20141018051107) do
     t.integer  "firm_id"
     t.string   "county"
     t.string   "docket_number"
+    t.integer  "total_hours"
   end
 
   create_table "clients", force: true do |t|

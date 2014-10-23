b90ae64ce8f90e98afcc45cff4cb74377d0de4ed
ravjohal <ravneetjohal@gmail.com>
2014-10-23 00:44:55 -0700

    [#80524928] seed and fixed a few bugs

--- app/models/injury.rb
+++ app/models/injury.rb
@@ -2,6 +2,7 @@ class Injury < ActiveRecord::Base
 
 	INJURY_TYPE = ['Open Wound/Bruise', 'Sprain/Strain', 'Soft Tissue Tear', 'Dislocation', 'Nerve Damage', 'Fracture', 'Amputation', 'Paralysis', 'Death']
 	REGION_TYPE = ['Skull', 'Face', 'Head', 'Neck', 'Back', 'Chest', 'Thorax', 'Shoulder', 'Arm', 'Arm - wrist', 'Arm - elbow', 'Hand', 'Fingers', 'Pelvis/Hip', 'Femur', 'Knee', 'Lower Leg', 'Ankle', 'Foot', 'Toes', 'Internal']
+	SURGERY_TYPE = ['Arthoscopic', 'Open']
 
 	belongs_to :medical
 	belongs_to :firm

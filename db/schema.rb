d383ac115691e0c0961e7128cf50a47adf2e67d8
bensmalley <bensmalley@bensmalley-3.local>
2014-10-23 22:20:46 -0700

    small css changes

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20141022113157) do
   enable_extension "plpgsql"
   enable_extension "hstore"
 
-
   create_table "case_documents", force: true do |t|
     t.integer  "case_id"
     t.integer  "document_id"
@@ -40,40 +39,39 @@ ActiveRecord::Schema.define(version: 20141022113157) do
   end
 
   create_table "cases", force: true do |t|
-    t.string   "name",          limit: 255
+    t.string   "name"
     t.integer  "case_number"
     t.text     "description"
     t.decimal  "medical_bills"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "case_type",     limit: 255
-    t.string   "subtype",       limit: 255
+    t.string   "case_type"
+    t.string   "subtype"
     t.integer  "user_id"
     t.date     "closing_date"
     t.string   "state",         limit: 2
-    t.string   "status",                    default: "0"
-    t.string   "court",         limit: 255
+    t.string   "status",                  default: "0"
+    t.string   "court"
     t.integer  "firm_id"
     t.string   "county"
     t.string   "docket_number"
     t.integer  "total_hours"
   end
 
-
   create_table "contacts", force: true do |t|
-    t.string   "first_name",         limit: 255
-    t.string   "middle_name",        limit: 255
-    t.string   "last_name",          limit: 255
-    t.string   "address",            limit: 255
-    t.string   "city",               limit: 255
-    t.string   "state",              limit: 255
-    t.string   "country",            limit: 255
-    t.string   "phone_number",       limit: 255
+    t.string   "first_name"
+    t.string   "middle_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
+    t.string   "phone_number"
     t.integer  "fax_number"
-    t.string   "email",              limit: 255
-    t.string   "gender",             limit: 255
+    t.string   "email"
+    t.string   "gender"
     t.integer  "age"
-    t.string   "type",               limit: 255
+    t.string   "type"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
@@ -87,26 +85,25 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.boolean  "felony_convictions"
     t.boolean  "last_ten_years"
     t.integer  "jury_likeability"
-    t.string   "witness_type",       limit: 255
-    t.string   "witness_subtype",    limit: 255
-    t.string   "witness_doctype",    limit: 255
-    t.string   "attorney_type",      limit: 255
-    t.string   "staff_type",         limit: 255
+    t.string   "witness_type"
+    t.string   "witness_subtype"
+    t.string   "witness_doctype"
+    t.string   "attorney_type"
+    t.string   "staff_type"
     t.integer  "event_id"
     t.integer  "firm_id"
     t.integer  "user_account_id"
     t.string   "company"
   end
 
-
   create_table "documents", force: true do |t|
-    t.string   "author",     limit: 255
-    t.string   "doc_type",   limit: 255
-    t.string   "template",   limit: 255
+    t.string   "author"
+    t.string   "doc_type"
+    t.string   "template"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
-    t.string   "document",   limit: 255
+    t.string   "document"
     t.integer  "firm_id"
   end
 
@@ -121,8 +118,8 @@ ActiveRecord::Schema.define(version: 20141022113157) do
   end
 
   create_table "events", force: true do |t|
-    t.string   "subject",            limit: 255
-    t.string   "location",           limit: 255
+    t.string   "subject"
+    t.string   "location"
     t.date     "date"
     t.time     "time"
     t.boolean  "all_day"
@@ -131,7 +128,6 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "owner_id"
-    t.integer  "firm_id"
     t.string   "google_id"
     t.string   "etag"
     t.string   "status"
@@ -144,18 +140,19 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.string   "visibility"
     t.string   "iCalUID"
     t.integer  "sequence"
+    t.integer  "firm_id"
     t.string   "google_calendar_id"
   end
 
   create_table "firms", force: true do |t|
-    t.string   "name",       limit: 255
-    t.string   "address",    limit: 255
-    t.string   "phone",      limit: 255
-    t.string   "fax",        limit: 255
+    t.string   "name"
+    t.string   "address"
+    t.string   "phone"
+    t.string   "fax"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "zip",        limit: 255
-    t.string   "tenant",     limit: 255
+    t.string   "zip"
+    t.string   "tenant"
   end
 
   create_table "google_calendars", force: true do |t|
@@ -176,29 +173,25 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.date     "incident_date"
     t.date     "statute_of_limitations"
     t.integer  "defendant_liability"
-    t.boolean  "alcohol_involved",                                           default: false
-    t.boolean  "weather_factor",                                             default: false
-    t.decimal  "property_damage",                    precision: 8, scale: 2
-    t.boolean  "airbag_deployed",                                            default: false
-    t.string   "speed",                  limit: 255
-    t.string   "police_report",          limit: 255
+    t.boolean  "alcohol_involved",                               default: false
+    t.boolean  "weather_factor",                                 default: false
+    t.decimal  "property_damage",        precision: 8, scale: 2
+    t.boolean  "airbag_deployed",                                default: false
+    t.string   "speed"
+    t.string   "police_report"
     t.integer  "case_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "insurance_provider",     limit: 255
+    t.string   "insurance_provider"
     t.integer  "firm_id"
     t.boolean  "towed"
     t.boolean  "complaint_at_scene"
   end
 
   create_table "injuries", force: true do |t|
-    t.string   "injury_type",        limit: 255
-    t.string   "region",             limit: 255
-    t.string   "code",               limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.integer  "medical_id"
-    t.integer  "firm_id"
+    t.string   "injury_type"
+    t.string   "region"
+    t.string   "code"
     t.boolean  "dominant_side"
     t.boolean  "joint_fracture"
     t.boolean  "displaced_fracture"
@@ -214,6 +207,10 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.boolean  "stitches"
     t.boolean  "future_surgery"
     t.decimal  "future_medicals"
+    t.integer  "firm_id"
+    t.integer  "medical_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.boolean  "prior_complaint"
   end
 
@@ -244,11 +241,10 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.datetime "updated_at"
     t.integer  "case_id"
     t.integer  "user_id"
-    t.string   "note_type",  limit: 255
-    t.string   "author",     limit: 255
+    t.string   "note_type"
+    t.string   "author"
     t.integer  "firm_id"
   end
-
 
   create_table "resolutions", force: true do |t|
     t.integer  "case_id"
@@ -261,9 +257,8 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.datetime "updated_at",        null: false
   end
 
-
   create_table "tasks", force: true do |t|
-    t.string   "name",           limit: 255
+    t.string   "name"
     t.date     "due_date"
     t.date     "completed"
     t.boolean  "sms_reminder"
@@ -275,7 +270,6 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.integer  "firm_id"
   end
 
-
   create_table "user_events", force: true do |t|
     t.integer  "user_id"
     t.integer  "event_id"
@@ -284,33 +278,33 @@ ActiveRecord::Schema.define(version: 20141022113157) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  limit: 255, default: "",                           null: false
-    t.string   "encrypted_password",     limit: 255, default: ""
-    t.string   "reset_password_token",   limit: 255
+    t.string   "email",                  default: "",                           null: false
+    t.string   "encrypted_password",     default: ""
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,                            null: false
+    t.integer  "sign_in_count",          default: 0,                            null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name",                   limit: 255
-    t.string   "confirmation_token",     limit: 255
+    t.string   "name"
+    t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email",      limit: 255
+    t.string   "unconfirmed_email"
     t.integer  "role"
-    t.boolean  "show_onboarding",                    default: true
-    t.string   "oauth_refresh_token",    limit: 255
-    t.string   "oauth_token",            limit: 255
+    t.boolean  "show_onboarding",        default: true
+    t.string   "oauth_refresh_token"
+    t.string   "oauth_token"
     t.datetime "oauth_expires_at"
-    t.string   "google_email",           limit: 255
+    t.string   "google_email"
     t.integer  "firm_id"
-    t.string   "first_name",             limit: 255
-    t.string   "last_name",              limit: 255
-    t.string   "time_zone",                          default: "Pacific Time (US & Canada)"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "time_zone",              default: "Pacific Time (US & Canada)"
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
@@ -318,7 +312,7 @@ ActiveRecord::Schema.define(version: 20141022113157) do
     t.integer  "invitation_limit"
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
-    t.integer  "invitations_count",                  default: 0
+    t.integer  "invitations_count",      default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
@@ -326,13 +320,5 @@ ActiveRecord::Schema.define(version: 20141022113157) do
   add_index "users", ["invitations_count"], name: "index_users_on_invitations_count", using: :btree
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
-  create_table "witnesses", force: true do |t|
-    t.string   "witness_type",    limit: 255
-    t.string   "witness_subtype", limit: 255
-    t.string   "witness_doctype", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end

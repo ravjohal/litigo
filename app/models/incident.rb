9abd555d67261ec7640836fb4af8aa16bd2c6799
ravjohal <ravneetjohal@gmail.com>
2014-10-23 22:58:50 -0700

    [#80524928] added seed data and added Self-insured

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,6 +1,6 @@
 class Incident < ActiveRecord::Base
   
-  INSURANCE_PROVIDERS = ['Allstate', 'State Farm', 'GIECO', 'Progressive', 'AAA', 'American Family', 'Farmers', 'Travelers', 'Esurance', 'Other']
+  INSURANCE_PROVIDERS = ['Allstate', 'State Farm', 'GIECO', 'Progressive', 'AAA', 'American Family', 'Farmers', 'Travelers', 'Esurance', 'Self-insured', 'Other']
 
   belongs_to :case
   belongs_to :firm

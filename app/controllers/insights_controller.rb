ab72048ff9b9530312301d27c1cc51c9e026ebbc
ravjohal <ravneetjohal@gmail.com>
2014-10-27 21:22:31 -0700

    undid insights, has to be for all firms

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -1,19 +1,17 @@
 class InsightsController < ApplicationController
   include StatesHelper
-  before_action :set_user, :set_firm
   
   before_filter :authenticate_user!
   before_action :set_user, :set_firm
   
   def index
-    firm_inj = @firm.injuries
-    @injuries = firm_inj.select(:injury_type).where("injury_type != ''").order(injury_type: :asc).distinct
-    @regions = firm_inj.select(:region).where("region != ''").order(region: :asc).distinct
+    @injuries = Injury.select(:injury_type).where("injury_type != ''").order(injury_type: :asc).distinct
+    @regions = Injury.select(:region).where("region != ''").order(region: :asc).distinct
     @us_states = us_states
   end
 
   def filter_cases
-    cases = @firm.cases
+    cases = Case.all
     render json: cases.to_json(include: [:resolution, :incident, {medical: {include: :injuries}}])
   end
 

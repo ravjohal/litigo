b7a50e0762a154f110d9c528d883ce922cd3f097
ravjohal <ravneetjohal@gmail.com>
2014-10-27 21:04:43 -0700

    scoped it to firm only for the data viz

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -1,17 +1,19 @@
 class InsightsController < ApplicationController
   include StatesHelper
+  before_action :set_user, :set_firm
   
   before_filter :authenticate_user!
   before_action :set_user, :set_firm
   
   def index
-    @injuries = Injury.select(:injury_type).where("injury_type != ''").order(injury_type: :asc).distinct
-    @regions = Injury.select(:region).where("region != ''").order(region: :asc).distinct
+    firm_inj = @firm.injuries
+    @injuries = firm_inj.select(:injury_type).where("injury_type != ''").order(injury_type: :asc).distinct
+    @regions = firm_inj.select(:region).where("region != ''").order(region: :asc).distinct
     @us_states = us_states
   end
 
   def filter_cases
-    cases = Case.all
+    cases = @firm.cases
     render json: cases.to_json(include: [:resolution, :incident, {medical: {include: :injuries}}])
   end
 

1833f3e16af9970b2752c5f4442c173136bc0091
Erik Evans <erikevans719@gmail.com>
2014-10-28 08:54:22 -0400

    fix 2), 3) and 4)

--- app/controllers/insights_controller.rb
+++ app/controllers/insights_controller.rb
@@ -11,8 +11,38 @@ class InsightsController < ApplicationController
   end
 
   def filter_cases
-    cases = Case.all
-    render json: cases.to_json(include: [:resolution, :incident, {medical: {include: :injuries}}])
+    cases = Case.includes(:resolution).includes(:incident).includes(medical: :injuries)
+    unless params[:state].blank?
+      cases = cases.where(cases: {state: params[:state]})
+    end
+    unless params[:court].blank?
+      cases = cases.where(cases: {court: params[:court]})
+    end
+    unless params[:injury_type].blank?
+      cases = cases.where(injuries: {injury_type: params[:injury_type]} )
+    end
+    unless params[:region].blank?
+      cases = cases.where(injuries: {region: params[:region]} )
+    end
+    unless params[:insurer].blank?
+      cases = cases.where(incidents: {insurance_provider: params[:insurer]} )
+    end
+
+    pie = cases.joins(:resolution).group('resolutions.resolution_type').count()
+    map = cases.joins(:resolution).group('cases.state').average("resolutions.resolution_amount")
+
+    render json: { 
+      lines: cases.to_json(
+                include: {
+                  :resolution => {
+                    only: [:resolution_amount]
+                  }
+                }, 
+                only: [:state, :court]
+              ),
+      pie: pie,
+      map: map
+    }
   end
 
-end   
+end

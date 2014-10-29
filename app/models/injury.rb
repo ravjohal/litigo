6fccf92314ebed7d9c4f188d752ea72c356dfb71
ravjohal <ravneetjohal@gmail.com>
2014-10-28 22:52:01 -0700

    fixed injury type array constant

--- app/models/injury.rb
+++ app/models/injury.rb
@@ -1,7 +1,7 @@
 class Injury < ActiveRecord::Base
 
-	INJURY_TYPE = ['Open Wound/Bruise', 'Sprain/Strain', 'Soft Tissue Tear', 'Dislocation', 'Nerve Damage', 'Fracture', 'Amputation', 'Paralysis', 'Death']
-	REGION_TYPE = ['Skull', 'Face', 'Head', 'Neck', 'Back', 'Chest', 'Thorax', 'Shoulder', 'Arm', 'Arm - wrist', 'Arm - elbow', 'Hand', 'Fingers', 'Pelvis/Hip', 'Femur', 'Knee', 'Lower Leg', 'Ankle', 'Foot', 'Toes', 'Internal']
+	INJURY_TYPE = ['Open Wound', 'Strain/Sprain', 'Soft Tissue Tear', 'Dislocation', 'Nerve Damage', 'Fracture', 'Amputation', 'Paralysis', 'Death']
+	REGION_TYPE = ['Skull', 'Face', 'Head', 'Neck', 'Back', 'Thorax', 'Shoulder', 'Arm', 'Arm - wrist', 'Arm - elbow', 'Hand', 'Fingers', 'Pelvis/Hip', 'Femur', 'Knee', 'Lower Leg', 'Ankle', 'Foot', 'Toes', 'Internal']
 	SURGERY_TYPE = ['Arthoscopic', 'Open']
 
 	belongs_to :medical

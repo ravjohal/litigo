25fc0b6aa97c9e0aa2aef2179c2efd386664d2d0
Artem <artem.suchov@gmail.com>
2014-10-29 12:12:16 +0200

    create contact(not case tabs) feature [finish #81528028]

--- features/case.feature
+++ features/case.feature
@@ -14,5 +14,14 @@ Feature: Case feature
     Then I verify required fields for case for user with email "artem.suchov@gmail.com"
 
   @javascript
-  Scenario: I can edit the details
+  Scenario: I can edit the details of the caise
     When I visit sign up page
+    Given I am a logged in user with email "artem.suchov@gmail.com" and password "password"
+    Then I get the confirmation email and confirm it
+    Then I should be logged in user
+    And when I fill in the modal window
+    And I should logged in
+    And I create a case step by step
+    And I verify required fields for case for user with email "artem.suchov@gmail.com"
+    And I change the case fields
+    Then I verify required fields for case for user with email "artem.suchov@gmail.com" are changed
--- features/step_definitions/contacts_steps.rb
+++ features/step_definitions/contacts_steps.rb
@@ -13,3 +13,19 @@ Then(/^The contact for user with email "(.*?)" should be saved to the db$/) do |
   expect(Contact.where(id: u.id).first.first_name).to eq 'Artem'
   expect(Contact.where(id: u.id).first.last_name).to eq 'Suchov'
 end
+
+Then(/^I create the contact trough the case management$/) do
+  click_on 'CASE MANAGEMENT'
+  click_on 'CONTACTS'
+  click_on 'NEW CONTACT'
+  fill_in 'contact_first_name', with: 'Artem'
+  fill_in 'contact_last_name', with: 'Such'
+  click_on 'Create Contact'
+  expect(page).to have_content('Contact was successfully created.')
+end
+
+Then(/^The contact for user with email "(.*?)" should be saved to the db with the right fields$/) do |arg1|
+  u = User.where(email: arg1).first
+  expect(Contact.where(id: u.id).first.first_name).to eq 'Artem'
+  expect(Contact.where(id: u.id).first.last_name).to eq 'Suchov'
+end

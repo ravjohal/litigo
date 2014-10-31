8aac65b6ae415848b5c3469a3b52d0dfe6194b1d
ravjohal <ravneetjohal@gmail.com>
2014-10-31 10:34:49 -0700

    fixed conflicts with schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+
 ActiveRecord::Schema.define(version: 20141028061241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-  enable_extension "hstore"
 
   create_table "case_documents", force: true do |t|
     t.integer  "case_id"

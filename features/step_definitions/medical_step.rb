783ce187d6b7ec22d7ad1e1305cb80b52b9721d2
Artem <artem.suchov@gmail.com>
2014-10-31 12:51:13 +0200

    add medical tab feature [finish #80800248]

--- features/step_definitions/medical_step.rb
+++ features/step_definitions/medical_step.rb
@@ -3,15 +3,17 @@ Then(/^I create the medical$/) do
   click_on 'CASES'
   click_on 'some'
   click_on 'MEDICAL'
-  find("option[value='Soft Tissue Tear']").click
-  find("option[value='Face']").click
-  click_on 'Create Injury'
-  expect(page).to have_content('Injury was successfully created.')
+  click_on 'Edit'
+  fill_in 'medical_total_med_bills', with: 2
+  fill_in 'medical_subrogated_amount', with: 3
+  click_on 'Update Medical'
+  expect(page).to have_content('Medical was successfully updated.')
 end
 
 Then(/^The medical for user with email "(.*?)" should be saved to the db$/) do |arg1|
   u = User.where(email: arg1).last
-  expect(Medical.where(id: u.id).last).to eq 'Face'
+  expect(Medical.find_by_id(u.id).total_med_bills).to eq 2
+  expect(Medical.find_by_id(u.id).subrogated_amount).to eq 3
 end
 
 Then(/^I create the injury$/) do

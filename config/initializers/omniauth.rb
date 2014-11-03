311f2bee2995907d7df4393060448f09a17afddf
ravjohal <ravneetjohal@gmail.com>
2014-11-03 01:56:22 -0800

    [#81611718]

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,19 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/contacts.readonly',
-    redirect_uri: "http://`hostname`/auth/google_oauth2/callback", #TODO: make this a configurable host
+    redirect_uri: "http://#{gethostforurl}/auth/google_oauth2/callback", #TODO: make this a configurable host
     access_type: 'offline',
     prompt: 'consent'
   }
+end
+
+def gethostforurl
+	hostname_envs   = case
+    when Rails.env.production?
+      'litigo.co'
+    when Rails.env.staging?
+      'litigorav.herokuapp.com'
+    else
+      'localhost:3000'
+    end
 end

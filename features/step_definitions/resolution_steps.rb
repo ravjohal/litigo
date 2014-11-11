a7d9d3f75100ebb9bd002841f5aaa9e74c180898
Artem <artem.suchov@gmail.com>
2014-11-11 19:52:25 +0200

    fix cucumber tests [finish #82474494]

--- features/step_definitions/resolution_steps.rb
+++ features/step_definitions/resolution_steps.rb
@@ -10,14 +10,14 @@ When(/^I create the resolution$/) do
   fill_in 'resolution_settlement_demand', with: 1
   fill_in 'resolution_jury_demand', with: 3
   fill_in 'resolution_resolution_amount', with: 2
-  find("option[value='SETTLEMENTS']").click
-  click_on 'Update Resolution'
-  expect(page).to have_content('resolution was successfully updated.')
+  find("option[value='Settlement']").click
+  click_on 'Save'
+  expect(page).to have_content('Resolution successfully updated.')
 end
 
 Then(/^The resolution for user with email "(.*?)" should be saved to the db$/) do |arg1|
   u = User.where(email: arg1).first
-  expect(Resolution.where(id: u.id).first.resolution_type).to eq 'SETTLEMENTS'
+  expect(Resolution.where(id: u.id).first.resolution_type).to eq 'Settlement'
   expect(Resolution.where(id: u.id).first.settlement_demand).to eq 1
   expect(Resolution.where(id: u.id).first.jury_demand).to eq 3
   expect(Resolution.where(id: u.id).first.resolution_amount).to eq 2

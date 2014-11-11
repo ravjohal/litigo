60982975f2be6d21e6eb169b8af58ad30660d68c
Artem <artem.suchov@gmail.com>
2014-11-11 13:53:53 +0200

    config the assets [#82368906]

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.assets.precompile += %w( homepage.css)
-Rails.application.config.assets.precompile += %w( hopscotch.js)
+Rails.application.config.assets.precompile += %w( homepage.css )
+Rails.application.config.assets.precompile += %w( hopscotch.js jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js welcome_tour.js tag-it.js)

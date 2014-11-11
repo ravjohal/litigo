5b14305bf219852611403400bcbedc0fd7d1a2ed
ravjohal <ravneetjohal@gmail.com>
2014-11-11 01:14:38 -0800

    fixing migration issue again

--- db/migrate/20141109225218_change_police_report_to_boolean_in_incident.rb
+++ db/migrate/20141109225218_change_police_report_to_boolean_in_incident.rb
@@ -1,10 +1,10 @@
-
 class ChangePoliceReportToBooleanInIncident < ActiveRecord::Migration
   class << self
     include AlterColumn
   end
  
   def self.up
+  	Incident.where(police_report: "").update_all(police_report: false)
     alter_column :incidents, :police_report, :boolean, "USING CAST(police_report AS boolean)", false
   end
  

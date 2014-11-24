d9ccbf370c0f89d575da07b1043de50f8271ca93
Artem Suchov <artem.suchov@gmail.com>
2014-11-24 16:01:00 +0200

    fix tour [finish #82542828]

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.assets.precompile += %w( homepage.css )
-Rails.application.config.assets.precompile += %w( hopscotch.js jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js welcome_tour.js tag-it.js)
+Rails.application.config.assets.precompile += %w( jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js tag-it.js)

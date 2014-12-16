b0b2ae62ef367af5e75c9c0cc87ed478b09c1eb0
ravjohal <ravneetjohal@gmail.com>
2014-12-15 23:48:45 -0800

    add my documents table [deliver #84092990]

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -8,10 +8,12 @@ class DocumentsController < ApplicationController
   def index
     if get_case
       @documents = @case.documents
+      @my_documents = @documents.joins(:cases => [{:contacts => :user}]).where(:contacts => {:contact_user_id => @user.id})
       @new_path = new_case_document_path(@case)
       @documents_a = [@case, Document.new] #for modal partial rendering
     else
       @documents = @firm.documents
+      @my_documents = @documents.joins(:cases => [{:contacts => :user}]).where(:contacts => {:contact_user_id => @user.id})
       @new_path = new_document_path
       @documents_a = Document.new #for modal partial rendering
     end

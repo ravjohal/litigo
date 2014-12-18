d38d1a4bf4bb3f75e20c04ce8b8bac6fc0aad180
Andrew Goncharenko <answer2005@gmail.com>
2014-12-18 14:45:11 +0200

    [finish #84418166] feedback show on every page bug fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,15 +53,7 @@
 //= require bootstrap-datetimepicker
 //= require_tree .
 
-
-$(document).ready(function () {
-
-    // $(window).on('hashchange', function(e){
-    //     console.log("hola" + e);
-    // });
-    // console.log("weeeee" );
-
-    //set up some minimal options for the feedback_me plugin
+var ready = function(){
     fm_options = {
         show_email: true,
         email_required: true,
@@ -83,61 +75,15 @@ $(document).ready(function () {
             send_success : "Sent successfully :) , now go ahead and star/watch the project"
         }
     };
-
-        //init feedback_me plugin
-
-// every page has class welcome-link on the page somewhere when the USER is Logged IN
-// when not logged in the class does not exsist
     if ($(".welcome-link").length !== 0) {
-       fm.init(fm_options);
+        fm.init(fm_options);
+    }
+};
 
-      }
-});
-
+$(document).ready(ready);
+$(document).on('page:load', ready);
 $(document).on('page:change', function (e) {
     if(e.currentTarget.location.pathname == "/insights") {
         $("#btnFilterSearch").click();
     }
 });
-
-// var default_options = {
-//     feedback_url: "",
-//     position: "left-top",
-//     jQueryUI: false,
-//     bootstrap: false,
-//     show_email: false,
-//     show_radio_button_list: false,
-//     close_on_click_outisde: true,
-//     name_label: "Name",
-//     email_label: "Email",
-//     message_label: "Message",
-//     radio_button_list_labels: ["1", "2", "3", "4", "5"],
-//     radio_button_list_title: "How would you rate my site?",
-//     name_placeholder: "",
-//     email_placeholder: "",
-//     message_placeholder: "",
-//     name_required: false,
-//     email_required: false,
-//     message_required: false,
-//     radio_button_list_required: false,
-//     show_asterisk_for_required: false,
-//     submit_label: "Send",
-//     title_label: "Feedback",
-//     trigger_label: "Feedback",
-//     custom_params: {},
-//     iframe_url : undefined,
-//     show_form: true,
-//     custom_html: "",
-//     delayed_close : true,
-//     delayed_options : {
-//         delay_success_milliseconds : 2000,
-//         delay_fail_milliseconds : 2000,
-//         sending : "Sending...",
-//         send_fail : "Sending failed.",
-//         send_success : "Feedack sent.",
-//         fail_color : undefined,
-//         success_color : undefined
-//     }
-// };
-
-

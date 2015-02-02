2618c791b242b5a02efa0eed080b787f8db67cd4
Andrew Goncharenko <answer2005@gmail.com>
2015-02-02 18:05:09 +0200

    [finish #87081836] Add colored circles based on case status - done

--- app/datatables/cases_datatable.rb
+++ app/datatables/cases_datatable.rb
@@ -29,7 +29,7 @@ class CasesDatatable
           one_case.case_type,
           total_med_bills,
           content_tag(:div, one_case.description, class: "table-row-max-h").html_safe,
-          one_case.status
+          content_tag(:div, '', class: "status-circle #{Case::STATUS[one_case.status.to_sym]}", title: "#{one_case.status.capitalize}").html_safe,
       ]
     end
   end

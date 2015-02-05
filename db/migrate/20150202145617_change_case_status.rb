eb51c3e438be91dfe4482cc6314267f516a069bb
ravjohal <ravneetjohal@gmail.com>
2015-02-05 01:42:06 -0800

    conversion error on converting nil instance

--- db/migrate/20150202145617_change_case_status.rb
+++ db/migrate/20150202145617_change_case_status.rb
@@ -1,10 +1,12 @@
 class ChangeCaseStatus < ActiveRecord::Migration
   def change
     Case.all.each do |one_case|
-      if one_case.status.downcase  == 'open'
-        one_case.status = 'negotiation'
-      elsif one_case.status.downcase == 'pending'
-        one_case.status = 'litigation'
+      if one_case.status
+        if one_case.status.downcase  == 'open'
+          one_case.status = 'negotiation'
+        elsif one_case.status.downcase == 'pending'
+          one_case.status = 'litigation'
+        end
       end
       one_case.save!
     end

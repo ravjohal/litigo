2c673f8dccd356ecfacfb9f9cb90598b90ec5ed5
Andrew Goncharenko <answer2005@gmail.com>
2015-02-12 21:25:09 +0200

    [#88017628] bug fix

--- app/datatables/leads_datatable.rb
+++ app/datatables/leads_datatable.rb
@@ -65,7 +65,7 @@ class LeadsDatatable
   end
 
   def sort_column
-    columns = %w[created_at attorney injured_party_name estimated_value status]
+    columns = %w[created_at attorney first_name estimated_value status]
     columns[params[:iSortCol_0].to_i]
   end
 

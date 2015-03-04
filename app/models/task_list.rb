23cf05144816ff33e8bb662e61fcb6cd21059882
Andrew Goncharenko <answer2005@gmail.com>
2015-03-04 17:38:28 +0200

    [#84090554] create index page with import to case button

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -1,7 +1,10 @@
 class TaskList < ActiveRecord::Base
-	has_many :task_drafts
+	has_many :task_drafts, :dependent => :destroy
+  belongs_to :user
+  belongs_to :firm
   accepts_nested_attributes_for :task_drafts, :reject_if => :all_blank, :allow_destroy => true
   VIEW_PERMISSIONS = {all_firm: 'Everyone in the firm', author: 'Author Only'}
   AMEND_PERMISSIONS = {all_firm: 'Everyone in the firm', author: 'Author Only', admins: 'Admins Only', admins_attorneys: 'Admins and Attorneys Only'}
+  TASK_IMPORT = {automatic: 'Automatically import', manual: 'Manually import'}
   validates_presence_of :name
 end

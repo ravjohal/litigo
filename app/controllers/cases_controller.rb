846a815fd7cc780314cc689e017ea03f3b4c84b0
Andrew Goncharenko <answer2005@gmail.com>
2015-03-04 23:08:27 +0200

    [finish #84090554] add import task_list into case tasks

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -98,7 +98,7 @@ class CasesController < ApplicationController
     def case_params
       params.require(:case).permit(:name, :trial_date, :case_number, :docket_number, :description, :case_type, :subtype,
         :court, :county, :plaintiff, :defendant, :corporation, :status,
-        :creation_date, :closing_date, :state, :medical_bills, topic,
+        :creation_date, :closing_date, :state, :medical_bills, :topic,
         :medical_attributes => [:total_med_bills, :subrogated_amount, :injuries_within_three_days, :length_of_treatment, :doctor_type, :treatment_type, :created_at, :updated_at, :id, 
         :injuries_attributes => [:injury_type, :region, :code, :created_at, :updated_at, :primary_injury, :id]], 
         :incident_attributes => [:incident_date, :statute_of_limitations, :defendant_liability, :alcohol_involved, :weather_factor, :property_damage, :airbag_deployed, :speed, :police_report, :insurance_provider, :created_at, :updated_at, :id], 

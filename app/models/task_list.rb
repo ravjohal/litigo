c23484195507f3111c6b96bf69334579dc2680d1
bensmalley <bensmalley@bensmalley-3.local>
2015-03-14 20:46:43 -0700

    Task List Styling

--- app/models/task_list.rb
+++ app/models/task_list.rb
@@ -4,7 +4,7 @@ class TaskList < ActiveRecord::Base
   belongs_to :firm
   accepts_nested_attributes_for :task_drafts, :reject_if => :all_blank, :allow_destroy => true
   VIEW_PERMISSIONS = {all_firm: 'Everyone in the firm', author: 'Author Only'}
-  AMEND_PERMISSIONS = {all_firm: 'Everyone in the firm', author: 'Author Only', admins: 'Admins Only', admins_attorneys: 'Admins and Attorneys Only'}
-  TASK_IMPORT = {automatic: 'Automatically import', manual: 'Manually import'}
+  AMEND_PERMISSIONS = {all_firm: 'Everyone in the firm', admins_attorneys: 'Admins and Attorneys Only', admins: 'Administrators Only', author: 'Author Only'}
+  TASK_IMPORT = {automatic: 'Automatically import', manual: 'Manual import'}
   validates_presence_of :name
 end

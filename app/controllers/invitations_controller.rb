cde50c4c74713f7463346d7f62a26353fac6c801
ravjohal <ravneetjohal@gmail.com>
2015-03-17 21:30:21 -0700

    fixed conflicts on confirm_email

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -4,40 +4,65 @@ class InvitationsController < Devise::InvitationsController
     @firm = current_user.firm
   end
 
+  # Creates the invitation(s) (is called after the new action is taken)
   def create
     users_params = params[:users]
     emails = []
     users_params.each_value do |attrs|
       user = User.find_by(email: attrs['email'])
-      if user.present?
+      if !user.present?
+        user = User.new(email: attrs['email'], firm_id: current_user.firm_id, role: attrs['role'].to_i)
+      end
+        # if the user is in the system, then invite the person this way
+      #   @raw_invitation_token = Devise.token_generator.generate(User, :invitation_token)
+      #   logger.info "@raw_invitation_token: #{@raw_invitation_token}\n\n\n"
+      #   user.invitation_token = @raw_invitation_token[1]
+      #   user.invitation_created_at = Time.now.utc
+      #   user.invitation_sent_at = user.invitation_created_at
+      #   user.invitation_role = attrs['role'].to_i
+      #   user.invited_by = current_user
+      #   options = {user: user, admin: current_user, token: @raw_invitation_token[0], role: User::USER_ROLES[attrs['role'].to_i]}
+      #   # puts "ADMIN WHAT IS ADMIN ----------> " + options.inspect
+      #   UserEmails.invite_existing_user(options).deliver if user.save(:validate => false)
+      # else
+      #   # if the user is NOT in the system, then send out invite this way (it also creates a user)
+      #   puts "IS THIS WHERE I AM ---------------------> "
+      #   @raw_invitation_token = Devise.token_generator.generate(User, :invitation_token)
+      #   user = User.new(email: attrs['email'], firm_id: current_user.firm_id, role: attrs['role'].to_i)
+      #   options = {user: user, admin: current_user, token: @raw_invitation_token[0], role: [attrs['role'].to_i]}
+      #   #user.save!
+      #   UserEmails.invite_new_user(options).deliver if user.save(:validate => false)
+        #user = User.invite!({email: attrs['email'], firm_id: current_user.firm_id, role: attrs['role'].to_i }, current_user)
+      #end
         @raw_invitation_token = Devise.token_generator.generate(User, :invitation_token)
         logger.info "@raw_invitation_token: #{@raw_invitation_token}\n\n\n"
         user.invitation_token = @raw_invitation_token[1]
         user.invitation_created_at = Time.now.utc
         user.invitation_sent_at = user.invitation_created_at
         user.invitation_role = attrs['role'].to_i
-        puts "INVITATION ROLE ============> " + user.invitation_role.to_s
         user.invited_by = current_user
         options = {user: user, admin: current_user, token: @raw_invitation_token[0], role: User::USER_ROLES[attrs['role'].to_i]}
+        # puts "ADMIN WHAT IS ADMIN ----------> " + options.inspect
         UserEmails.invite_existing_user(options).deliver if user.save(:validate => false)
-      else
-        puts " I AM IN INVITATIONS AND WANT TO KNOW THE ROLE!!!!!!!!!!!!! " + attrs['role'].to_i.to_s + "  user: " + current_user.to_s
-        user = User.invite!({email: attrs['email'], firm_id: current_user.firm_id, role: attrs['role'].to_i }, current_user)
-      end
-      emails << attrs['email']
+
+        emails << attrs['email']
     end
 
     respond_to do |format|
       if emails.present?
         format.html { redirect_to root_path, notice: "Invitations were sent to: #{emails.to_sentence}" }
       else
-        flash[:notice] = "Email was sent"
+        flash[:notice] = "Email was not sent"
         format.html { render :new }
       end
     end
   end
 
   def edit
+    
+  end
+
+  def update
     invitation_token = Devise.token_generator.digest(User, :invitation_token, params[:invitation_token])
     user = User.find_by(invitation_token: invitation_token)
     if user.present? && user.encrypted_password.present?
@@ -53,13 +78,11 @@ class InvitationsController < Devise::InvitationsController
       redirect_to root_path, notice: 'Invitation accepted.'
     else
       super
+      # resource.name = resource.first_name + " " + resource.last_name
+      # resource.save
     end
-  end
+ #   super
 
-  def update
-    super
-    resource.name = resource.first_name + " " + resource.last_name
-    resource.save
 
     # class_string_name = User::USER_ROLES[current_user.role].humanize
     # puts "CONTACT TYPE ------> " + class_string_name
--- app/mailers/user_emails.rb
+++ app/mailers/user_emails.rb
@@ -1,4 +1,6 @@
 class UserEmails < ActionMailer::Base
+  include Devise::Mailers::Helpers
+  
   def invite_existing_user(options)
     @send_to = options[:user][:email]
     @user = options[:user]
@@ -10,6 +12,18 @@ class UserEmails < ActionMailer::Base
          subject: "Invitation instructions",
          content_type: "text/html") unless Rails.env.test?
   end
+
+  # def invite_new_user(options)
+  #   @send_to = options[:user][:email]
+  #   @user = options[:user]
+  #   @admin = options[:admin]
+  #   @token = options[:token]
+  #   @role = options[:role]
+  #   mail(to: @send_to,
+  #        from: "\"Litigo\" <ben@litigo.co>",
+  #        subject: "Invitation instructions",
+  #        content_type: "text/html") unless Rails.env.test?
+  # end
 
   def event_invitation(options)
     @user = options[:user]

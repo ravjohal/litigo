339cccffd8fc6919a14e85a4c0f133e4120a8dd8
bensmalley <bensmalley@bensmalley-3.local>
2015-03-20 19:11:57 -0700

    Random CSS

--- app/models/task_draft.rb
+++ app/models/task_draft.rb
@@ -9,33 +9,33 @@ class TaskDraft < ActiveRecord::Base
   accepts_nested_attributes_for :children, :reject_if => :all_blank, :allow_destroy => true
   validates :due_term, :numericality => { :greater_than_or_equal_to => 0 }
 
-  ANCHOR_DATE = ['close date', 'case open', 'incident date', 'statute of limitations', 'trial date']
-  BEFORE_ANCHOR_DATE = ['close date', 'statute of limitations', 'trial date']
+  ANCHOR_DATE = ['Case Open Date', 'Incident Date', 'Statute of Limitations', 'Trial Date','Case Close Date']
+  BEFORE_ANCHOR_DATE = ['Statute of Limitations', 'Trial Date', 'Case Close Date']
 
   def return_due_date(affair, parent=nil)
     due_date = nil
     if self.conjunction.downcase == 'after'
       case self.anchor_date
-        when 'case open'
+        when 'Case Open Date'
           due_date = affair.created_at + self.due_term.days
-        when 'incident date'
+        when 'Incident Date'
           due_date = affair.incident.incident_date + self.due_term.days if affair.incident.incident_date.present?
-        when 'statute of limitations'
+        when 'Statute of Limitations'
           due_date = affair.incident.statute_of_limitations + self.due_term.days if affair.incident.incident_date.present?
-        when 'trial date'
+        when 'Trial Date'
           due_date = affair.trial_date + self.due_term.days if affair.trial_date.present?
-        when 'parent'
+        when 'Parent'
           due_date = parent.due_date + self.due_term.days if parent.due_date.present?
-        when 'close date'
+        when 'Case Close Date'
           due_date = affair.closing_date + self.due_term.days if affair.closing_date.present?
       end
     elsif self.conjunction.downcase == 'before'
       case self.anchor_date
-        when 'trial date'
+        when 'Trial Date'
           due_date = affair.trial_date - self.due_term.days if affair.trial_date.present?
-        when 'statute of limitations'
+        when 'Statute of Limitations'
           due_date = incident.statute_of_limitations - self.due_term.days if incident.incident_date.present?
-        when 'close date'
+        when 'Case Close Date'
           due_date = affair.closing_date - self.due_term.days if affair.closing_date.present?
         when 'previous task'
           due_date = parent.due_date - self.due_term.days if parent.due_date.present?

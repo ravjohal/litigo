3628a16b6129ac34669db00db640f49c4a22fd78
ravjohal <ravneetjohal@gmail.com>
2015-04-18 13:05:36 -0700

    [deliver #87478592]

--- app/models/medical.rb
+++ app/models/medical.rb
@@ -9,5 +9,5 @@ class Medical < ActiveRecord::Base
 	belongs_to :firm
 
 	accepts_nested_attributes_for :injuries, :allow_destroy => true
-	accepts_nested_attributes_for :medical_bills, :allow_destroy => true
+	accepts_nested_attributes_for :medical_bills, :reject_if => :all_blank, :allow_destroy => true
 end

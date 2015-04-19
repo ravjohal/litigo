b61c3606fc5f10d73df099d62f0d8922cbd52758
ravjohal <ravneetjohal@gmail.com>
2015-04-19 05:21:48 -0700

    [#92771382]

--- app/reports/case_sol_report.rb
+++ app/reports/case_sol_report.rb
@@ -1,15 +1,19 @@
 class CaseSolReport < Dossier::Report
  
   def sql
-    Case.where("").select("number, name, sub_type, ").to_sql
+    Case.where("firm_id = :firm_id AND cast(statute_of_limitations as date) between :start_date and :end_date").select("case_number, name, subtype").to_sql
+  end
+
+  def firm_id
+	options[:firm_id]
   end
 
   def start_date
-
+	options[:start_date]
   end
 
   def end_date
-
+	options[:end_date]
   end
 
 end

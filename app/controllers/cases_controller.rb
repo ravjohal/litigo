f69ef5d4782b63030d4287915f5f9207c659ebe2
ravjohal <ravneetjohal@gmail.com>
2015-04-19 12:20:25 -0700

    [#92771382]

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -120,10 +120,10 @@ class CasesController < ApplicationController
     def case_params
       params.require(:case).permit(:name, :trial_date, :case_number, :docket_number, :description, :case_type, :subtype,
         :court, :county, :plaintiff, :defendant, :corporation, :status, :hearing_date, :filed_suit_date,
-        :creation_date, :closing_date, :state, :medical_bills, :topic, :transfer_date,
+        :creation_date, :closing_date, :state, :medical_bills, :topic, :transfer_date, :statute_of_limitations,
         :medical_attributes => [:total_med_bills, :subrogated_amount, :injuries_within_three_days, :length_of_treatment, :doctor_type, :treatment_type, :created_at, :updated_at, :id, 
         :injuries_attributes => [:injury_type, :region, :code, :created_at, :updated_at, :primary_injury, :id]], 
-        :incident_attributes => [:incident_date, :statute_of_limitations, :defendant_liability, :alcohol_involved, :weather_factor, :property_damage, :airbag_deployed, :speed, :police_report, :created_at, :updated_at, :id], 
+        :incident_attributes => [:incident_date, :defendant_liability, :alcohol_involved, :weather_factor, :property_damage, :airbag_deployed, :speed, :police_report, :created_at, :updated_at, :id], 
         :insurance_attributes => [:parent_id, :insurance_type, :insurance_provider, :policy_limit, :claim_number, :policy_holder, :created_at, :updated_at, :id, :case_id, :_destroy,
         :children_attributes => [:parent_id, :insurance_type, :insurance_provider, :policy_limit, :claim_number, :policy_holder, :created_at, :updated_at, :id, :case_id, :_destroy]], 
         :resolution_attributes => [:id, :updated_at, :created_at, :firm_id, :settlement_demand, :jury_demand, :resolution_amount, :resolution_type], 

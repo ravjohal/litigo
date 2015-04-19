f43428cfa639e9b1ebe9381ddcd4ace72180a96b
ravjohal <ravneetjohal@gmail.com>
2015-04-19 04:54:50 -0700

    inclusive date issue fixed

--- app/reports/leads_detail_report.rb
+++ app/reports/leads_detail_report.rb
@@ -1,6 +1,6 @@
 class LeadsDetailReport < Dossier::Report
 	def sql
-		Lead.where("marketing_channel = :marketing_channel_arg AND created_at between :start_date and :end_date").select("marketing_channel, first_name, last_name, created_at, sub_type, estimated_value").to_sql
+		Lead.where("marketing_channel = :marketing_channel_arg AND cast(created_at as date) between :start_date and :end_date").select("marketing_channel, first_name, last_name, created_at, sub_type, estimated_value").to_sql
 	end
 
 	def start_date

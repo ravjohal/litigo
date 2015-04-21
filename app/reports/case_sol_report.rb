d55a8afdfba5eb6fed610bfd59c2b7efdc972383
ravjohal <ravneetjohal@gmail.com>
2015-04-21 01:15:04 -0700

    [#92771382] - updated the SOL Report

--- app/reports/case_sol_report.rb
+++ app/reports/case_sol_report.rb
@@ -1,7 +1,31 @@
 class CaseSolReport < Dossier::Report
  #@my_notes = @notes.joins(:case => [{:contacts => :user}]).where(:contacts => {:contact_user_id => @user.id})
+
+# ABOVE NOTATION TRANSLATES TO EXACT SQL:
+
+ # SELECT "notes".* FROM "notes" 
+ # INNER JOIN "cases" ON "cases"."id" = "notes"."case_id" 
+ # INNER JOIN "case_contacts" ON "case_contacts"."case_id" = "cases"."id" 
+ # INNER JOIN "contacts" ON "contacts"."id" = "case_contacts"."contact_id" 
+ # INNER JOIN "users" ON "users"."id" = "contacts"."user_id" 
+ # WHERE "notes"."firm_id" = $1 AND "contacts"."contact_user_id" = 23
+
   def sql
-    Case.where("firm_id = :firm_id AND cast(statute_of_limitations as date) between :start_date and :end_date").select("case_number, name, subtype").to_sql
+    Case.joins(:contacts, :incident).where("cases.firm_id = :firm_id 
+    	AND type IN ('Attorney', 'Staff')
+    	AND contacts.id = :attorney_contact_id 
+    	AND cast(statute_of_limitations as date) between :start_date and :end_date"
+    	).select("case_number, name, subtype, first_name, last_name, incident_date, statute_of_limitations, filed_suit_date"
+    	).to_sql
+
+ # SELECT case_number, name, subtype, first_name, last_name, incident_date, statute_of_limitations, filed_suit_date 
+ # FROM "cases" INNER JOIN "case_contacts" ON "case_contacts"."case_id" = "cases"."id" 
+ # INNER JOIN "contacts" ON "contacts"."id" = "case_contacts"."contact_id" 
+ # INNER JOIN "incidents" ON "incidents"."case_id" = "cases"."id" 
+ # WHERE (cases.firm_id = 16 
+ #   	AND type IN ('Attorney', 'Staff')
+ #   	AND contacts.id = '1874' 
+ #   	AND cast(statute_of_limitations as date) between '2015-04-01' and '2015-04-30')
   end
 
   def firm_id
@@ -23,6 +47,10 @@ class CaseSolReport < Dossier::Report
 	else
 		options[:end_date]
 	end
+  end
+
+  def attorney_contact_id
+  	options[:attorney_contact_id]
   end
 
 end

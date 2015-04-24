1a9bf5154ef1bc389ec79943aa611368f0f04499
ravjohal <ravneetjohal@gmail.com>
2015-04-24 00:36:27 -0700

    [deliver #93024218] and fixed date of intake bug

--- app/models/medical_bill.rb
+++ app/models/medical_bill.rb
@@ -1,4 +1,6 @@
 class MedicalBill < ActiveRecord::Base
+	before_save :paid_amount_negative
+
 	belongs_to :user
 	belongs_to :firm
 	belongs_to :medical
@@ -7,4 +9,8 @@ class MedicalBill < ActiveRecord::Base
  #  	belongs_to :parent, class_name: "MedicalBill"
 
 	#accepts_nested_attributes_for :children, :reject_if => :all_blank, :allow_destroy => true
+
+	def paid_amount_negative
+		self.paid_amount = -(self.paid_amount.abs)
+	end
 end

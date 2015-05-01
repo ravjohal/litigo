1467ceb479c3930a8e49173589b212205b6f4384
ravjohal <ravneetjohal@gmail.com>
2015-05-01 01:48:25 -0700

    [deliver #93151776]

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,20 @@ class Company < ActiveRecord::Base
 	belongs_to :user
 
 	accepts_nested_attributes_for :contacts, :reject_if => :all_blank
+
+	def company_city_and_state
+		if !self.city.empty? && !self.state.empty?
+			self.city + ", " + self.state
+		end
+    end
+
+    def company_city_or_state
+    	if self.city.empty? && !self.state.empty?
+    		self.state
+    	elsif self.state.empty? && !self.city.empty?
+    		self.city
+    	else
+    		self.company_city_and_state
+    	end
+    end
 end

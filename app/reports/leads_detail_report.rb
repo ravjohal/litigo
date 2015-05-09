a2d73a58c373a5f9a037bc0120b891f4cc2ceb05
ravjohal <ravneetjohal@gmail.com>
2015-05-08 23:14:47 -0700

    [deliver #92771382]

--- app/reports/leads_detail_report.rb
+++ app/reports/leads_detail_report.rb
@@ -1,6 +1,10 @@
 class LeadsDetailReport < Dossier::Report
 	def sql
-		Lead.where("firm_id = :firm_id AND marketing_channel IN :marketing_channel_arg AND cast(created_at as date) between :start_date and :end_date").select("marketing_channel, first_name, last_name, cast(created_at AS date), sub_type, cast(estimated_value AS money)").to_sql
+		Lead.where("firm_id = :firm_id 
+			AND marketing_channel IN :marketing_channel_arg 
+			AND cast(created_at as date) between :start_date and :end_date"
+			).select("marketing_channel, first_name, last_name, cast(created_at AS date), sub_type, cast(estimated_value AS money)"
+			).to_sql
 	end
 
 	def firm_id

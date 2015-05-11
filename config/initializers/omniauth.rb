e5a24f1a55e91c93f837438b5cc5405464103bde
Andrew Goncharenko <answer2005@gmail.com>
2015-05-11 11:07:54 +0300

    [finish #94261912] try to fix bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"], {
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/contacts.readonly',
-    redirect_uri: "http://#{gethostforurl}/auth/google_oauth2/callback", #TODO: make this a configurable host
+    redirect_uri: "#{gethostforurl}/auth/google_oauth2/callback", #TODO: make this a configurable host
     access_type: 'offline',
     prompt: 'consent'
   }
@@ -10,7 +10,7 @@ end
 def gethostforurl
 	hostname_envs   = case
     when Rails.env.production?
-      'www.litigo.co'
+      'https://www.litigo.co'
     when Rails.env.staging?
       'litigorav.herokuapp.com'
     else

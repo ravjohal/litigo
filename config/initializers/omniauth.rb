1e93f8457187aab41bb907c8af13db853992d373
Andrew Goncharenko <answer2005@gmail.com>
2015-05-12 14:51:19 +0300

    [#94261912] try to fix bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/contacts.readonly',
     redirect_uri: "#{gethostforurl}/auth/google_oauth2/callback", #TODO: make this a configurable host
     access_type: 'offline',
-    prompt: 'consent'
+    prompt: 'consent',
+    approval_prompt: 'force'
   }
 end
 

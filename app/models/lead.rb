0dfb5a4b914a3d0405a509ecadf588a2824401a9
bensmalley <bensmalley@bensmalley-3.local>
2015-05-14 16:06:21 -0700

    Added Television to form options
    [#94622546]

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -5,7 +5,7 @@ class Lead < ActiveRecord::Base
   has_one :case
   has_many :documents
 
-  CHANNELS = ['Google', 'Word of mouth', 'Referral', 'Radio', 'Newspaper', 'Other']
+  CHANNELS = ['Google', 'Television', 'Word of mouth', 'Referral', 'Radio', 'Newspaper', 'Other']
   STATUS = {pending_review: "New Lead - Pending Review", appointment_scheduled: "Appt. Scheduled", rejected: "Rejected", accepted: "Accepted", inactive: "Inactive"}
 
   include PgSearch

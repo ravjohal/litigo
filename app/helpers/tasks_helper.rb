8f5bab65df3b3518691adfcbed02fdf15a4790cf
Andrew Goncharenko <answer2005@gmail.com>
2015-05-19 17:02:43 +0300

    try to fix production google calendar sync bug

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,5 @@
 module TasksHelper
   def user_google_calendars
-    @user.google_calendars.collect { |c| [ "<!--email_off--> #{c.google_id} <!--/email_off-->".html_safe, c.google_id] }
+    @user.google_calendars.collect { |c| [ "<!--email_off--> #{c.summary} <!--/email_off-->".html_safe, c.google_id] }
   end
 end

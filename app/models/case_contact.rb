4c6ab4d8920a43c66f6677b9824c121e078df4dc
Andrew Goncharenko <answer2005@gmail.com>
2015-05-22 02:52:19 +0300

    Contact creation role fix

--- app/models/case_contact.rb
+++ app/models/case_contact.rb
@@ -1,5 +1,11 @@
 class CaseContact < ActiveRecord::Base
 	belongs_to :affair, class_name: 'Case', :foreign_key => 'case_id'
 	belongs_to :contact
-	# belongs_to :plaintiff, :foreign_key => :contact_id
+	belongs_to :plaintiff, :foreign_key => :contact_id
+  before_create :set_role
+
+  def set_role
+    self.role = self.contact.type.to_s
+  end
+
 end

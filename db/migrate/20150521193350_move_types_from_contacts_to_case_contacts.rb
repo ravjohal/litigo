853fc84bde28841cf725f7ce4fbcd304985324a8
Andrew Goncharenko <answer2005@gmail.com>
2015-05-22 09:23:34 +0300

    fix migration

--- db/migrate/20150521193350_move_types_from_contacts_to_case_contacts.rb
+++ db/migrate/20150521193350_move_types_from_contacts_to_case_contacts.rb
@@ -2,7 +2,7 @@ class MoveTypesFromContactsToCaseContacts < ActiveRecord::Migration
   def change
     CaseContact.reset_column_information
     CaseContact.all.each do |case_contact|
-      case_contact.role = case_contact.contact.type.to_s
+      case_contact.role = case_contact.contact.type.to_s if case_contact.contact.type.present?
       case_contact.save
     end
   end

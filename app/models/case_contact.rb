fb2f269ca30ac9e33582d713e0cff71694c6dd20
Andrew Goncharenko <answer2005@gmail.com>
2015-05-26 16:31:47 +0300

    try to fix company contact bug

--- app/models/case_contact.rb
+++ app/models/case_contact.rb
@@ -2,7 +2,7 @@ class CaseContact < ActiveRecord::Base
 	belongs_to :affair, class_name: 'Case', :foreign_key => 'case_id'
 	belongs_to :contact
 	belongs_to :plaintiff, :foreign_key => :contact_id
-  
+
   #before_create :set_role
 
   def set_role

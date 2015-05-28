9b3a5e58b5e1ba1e0436cb10d9c1f1ac92691ef2
Andrew Goncharenko <answer2005@gmail.com>
2015-05-29 01:25:55 +0300

    [finish #95379624] SOL calculation - fixed

--- app/models/case_contact.rb
+++ app/models/case_contact.rb
@@ -2,11 +2,21 @@ class CaseContact < ActiveRecord::Base
 	belongs_to :affair, class_name: 'Case', :foreign_key => 'case_id'
 	belongs_to :contact
 	belongs_to :plaintiff, :foreign_key => :contact_id
-
+  after_save :check_sol
   #before_create :set_role
 
   def set_role
     self.role = self.contact.type.to_s
+  end
+
+
+  def check_sol
+    if self.role == 'Plaintiff'
+      if self.affair.present? && (self.contact.date_of_death.present? || self.contact.major_date.present?)
+        options = {date_of_death_changed: self.contact.date_of_death.present?, major_date_changed: self.contact.major_date.present?}
+        self.affair.calculate_sol(self.contact, options)
+      end
+    end
   end
 
 end

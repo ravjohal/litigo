8c0f45bd6f6bc3f306b1ecf2989eb997a48b5cc1
ravjohal <ravneetjohal@gmail.com>
2015-07-20 11:21:55 +0100

    [deliver #99343928]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150624124329) do
+ActiveRecord::Schema.define(version: 20150720100837) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -277,6 +277,8 @@ ActiveRecord::Schema.define(version: 20150624124329) do
     t.boolean  "recur",              default: false
     t.integer  "event_series_id"
     t.boolean  "all_day"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "expenses", force: :cascade do |t|

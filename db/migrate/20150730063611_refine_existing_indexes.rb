d6f59c50d253af773b44eb3717b6b9c120006fa3
ravjohal <ravneetjohal@gmail.com>
2015-07-30 00:21:52 -0700

    refined existing indexes

---
+++ db/migrate/20150730063611_refine_existing_indexes.rb
@@ -0,0 +1,39 @@
+class RefineExistingIndexes < ActiveRecord::Migration
+  def up
+  	# medical_bills
+  	remove_index :medical_bills, :user_id
+  	remove_index :medical_bills, :parent_id
+  	remove_index :medical_bills, :company_id
+  	add_index :medical_bills, :parent_id, where: "parent_id NOT NULL"
+  	add_index :medical_bills, :company_id, where: "company_id NOT NULL"
+
+  	# insurances
+  	add_index :insurances, :firm_id
+  	add_index :insurances, :company_id
+
+  	# contacts
+  	remove_index :contacts, :event_id
+
+  	# tasks
+  	remove_index :tasks, :parent_id
+  end
+
+  def down
+  	# medical_bills
+  	remove_index :medical_bills, :parent_id
+  	remove_index :medical_bills, :company_id
+  	add_index :medical_bills, :user_id
+  	add_index :medical_bills, :parent_id
+  	add_index :medical_bills, :company_id
+
+  	# insurances
+  	remove_index :insurances, :firm_id
+  	remove_index :insurances, :company_id
+
+  	# contacts
+  	add_index :contacts, :event_id
+
+  	# tasks
+  	add_index :tasks, :parent_id  	
+  end
+end

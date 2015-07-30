b926ce7f2f2b4b11be4b09533fb2780ecc3652cc
ravjohal <ravneetjohal@gmail.com>
2015-07-30 00:48:59 -0700

    fixed migration

--- db/migrate/20150730063611_refine_existing_indexes.rb
+++ db/migrate/20150730063611_refine_existing_indexes.rb
@@ -4,8 +4,8 @@ class RefineExistingIndexes < ActiveRecord::Migration
   	remove_index :medical_bills, :user_id
   	remove_index :medical_bills, :parent_id
   	remove_index :medical_bills, :company_id
-  	add_index :medical_bills, :parent_id, where: "parent_id NOT NULL"
-  	add_index :medical_bills, :company_id, where: "company_id NOT NULL"
+  	add_index :medical_bills, :parent_id, where: "parent_id <> NULL"
+  	add_index :medical_bills, :company_id, where: "company_id <> NULL"
 
   	# insurances
   	add_index :insurances, :firm_id

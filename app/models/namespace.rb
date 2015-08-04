e875097389bc0526cef12f730a81bdcbdc96b29d
dmitriy <dimmon908@gmail.com>
2015-08-04 12:10:27 +0300

    [#99776306] Add possibility to delete namespace with related calendars and events

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -2,6 +2,7 @@ class Namespace < ActiveRecord::Base
   belongs_to :user
   belongs_to :firm
   has_many :calendars, :dependent => :destroy
+  has_many :events, :dependent => :destroy
   SYNC_PERIODS = {'3' => 'Sync last 3 months', '6'=> 'Sync last 6 months', '12' => 'Sync Last year', '0' => 'Sync all calendar events'}
 
   def full_name

6c6cdfa6d781cc3d3bac118243de13aff3a0fbf0
dmitriy <dimmon908@gmail.com>
2015-08-11 10:50:35 +0300

    [#100909146] Implementing delete account and namespace from nylas by API, but it not work

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -7,7 +7,30 @@ class Namespace < ActiveRecord::Base
 
   include ActiveCalendars
 
+  before_destroy :delete_from_nylas
+
+  # @return [Inbox::Api]
+  def nylas_inbox
+    @nylas_api ||= Inbox::API.new(Rails.application.secrets.inbox_app_id, Rails.application.secrets.inbox_app_secret, inbox_token)
+  end
+
+  # @return [Inbox::RestfulModel]
+  def nylas_namespace
+    @nylas_namespace ||= nylas_inbox.namespaces.first
+  end
+
+  def nylas_account
+    @nylas_account ||= nylas_inbox.accounts.first
+  end
+
   def full_name
     name.present? ? "#{name}/#{provider}" : email_address
   end
+
+  private
+
+    def delete_from_nylas
+      nylas_account.destroy rescue nil
+      nylas_namespace.destroy rescue nil
+    end
 end

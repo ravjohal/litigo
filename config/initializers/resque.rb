511a6a6a6542ea24324d2fdaa3e6e6364e8fe5e2
ravjohal <ravneetjohal@gmail.com>
2015-08-14 04:13:24 -0700

    more setup to run bg jobs in heroku

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,3 +2,5 @@ if Rails.env.staging? || Rails.env.production?
   uri = URI.parse ENV['REDISTOGO_URL']
   Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
 end
+
+Dir["/app/app/workers/*.rb"].each { |file| require file }

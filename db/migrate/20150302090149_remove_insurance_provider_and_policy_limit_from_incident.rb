ce4f81b41b3ceb3d5dce98ffeb80711effe4f398
ravjohal <ravneetjohal@gmail.com>
2015-09-03 02:10:14 -0700

    more migration cleanup

--- db/migrate/20150302090149_remove_insurance_provider_and_policy_limit_from_incident.rb
+++
@@ -1,6 +0,0 @@
-class RemoveInsuranceProviderAndPolicyLimitFromIncident < ActiveRecord::Migration
-  def change
-  	remove_column :incidents, :insurance_provider
-  	remove_column :incidents, :policy_limit
-  end
-end

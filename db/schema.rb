5be33a7d15a7862bae4315c6817d51e848a51927
ravjohal <ravneetjohal@gmail.com>
2015-09-08 23:04:32 -0700

    [deliver #102965394] - soft delete users and do not allow them to sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150828062622) do
+ActiveRecord::Schema.define(version: 20150909045000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -586,6 +586,14 @@ ActiveRecord::Schema.define(version: 20150828062622) do
   add_index "notes", ["firm_id"], name: "index_notes_on_firm_id", using: :btree
   add_index "notes", ["user_id"], name: "index_notes_on_user_id", using: :btree
 
+  create_table "notes_users", force: :cascade do |t|
+    t.integer  "note_id"
+    t.integer  "user_id"
+    t.boolean  "is_author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "old_events", force: :cascade do |t|
     t.string   "subject",              limit: 255
     t.string   "location",             limit: 255
@@ -829,6 +837,7 @@ ActiveRecord::Schema.define(version: 20150828062622) do
     t.string   "middle_name"
     t.string   "events_color"
     t.boolean  "edit_events_permit",                 default: false
+    t.boolean  "disabled",                           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree

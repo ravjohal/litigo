7d4dfa8dbd663d0a756721c5de72bc5cfefb176a
Slava <fender707@rambler.ru>
2015-09-21 18:11:24 +0300

    [#103765160]- Move Tasks Javascript to Assets folder

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.assets.precompile += %w( homepage.css .woff .ttf)
-Rails.application.config.assets.precompile += %w( jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js tag-it.js)
+Rails.application.config.assets.precompile += %w( jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js tag-it.js tasks/show.js tasks/edit.js tasks/form.js)

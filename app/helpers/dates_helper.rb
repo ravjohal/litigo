5f99844902cdbfdb5ecf24db4fe28a19bdecab1e
Slava <fender707@rambler.ru>
2015-09-22 15:53:34 +0300

    [#100262978]- Added functions with regular expression for replace Strftime

--- app/helpers/dates_helper.rb
+++ app/helpers/dates_helper.rb
@@ -1,5 +1,43 @@
 module DatesHelper
 
+  MONTH = {
+      1 => 'Jan',
+      2 => 'Feb',
+      3 => 'Mar',
+      4 => 'Apr',
+      5 => 'May',
+      6 => 'June',
+      7 => 'July',
+      8 => 'Aug',
+      9 => 'Sep',
+      10 => 'Oct',
+      11 => 'Nov',
+      12 => 'Dec'
+  }
+
+  def reg_exp_date(date)
+    date_re = /^\d{4}\-\d{2}\-\d{2}/
+    date_re.match(date.to_s.split(' ').first).to_s.split('-')
+  end
+
+  def simple_format_datetime_regexp(date)
+    match = reg_exp_date(date)
+    [MONTH[match[1].to_i], match[2], match[0], date.to_s.split(' ')[1]].join(' ')
+  end
+
+  def simple_format_date_regexp(date)
+    match = reg_exp_date(date)
+    [MONTH[match[1].to_i], match[2], match[0]].join(' ')
+  end
+
+  def date_to_input_regexp(date)
+    match = reg_exp_date(date)
+    [match[1], match[2], match[0]].join('/')
+  end
+
+  def time_to_input_regexp(date)
+    date.to_s.split(' ')[1]
+  end
   def simple_format_date(date)
     date.try(:strftime, '%b %e, %Y')
   end

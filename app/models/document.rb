8ccb295215e90254931c0f2dbb544b0750823e40
dimmon908 <dimmon908@gmail.com>
2015-09-23 12:01:44 +0300

    [#100909590][#102343934] Implementing documents preview functionality for docx, images and pdf

--- app/models/document.rb
+++ app/models/document.rb
@@ -11,6 +11,57 @@ class Document < ActiveRecord::Base
 
 	before_destroy :clean_s3
 
+	def file_extension
+		document.try(:file).try(:extension)
+	end
+
+	def can_preview?
+		document.try(:file).try(:extension)
+	end
+
+	def image?
+		%w(png jpg jpeg bmp).include? file_extension
+	end
+
+	def docx?
+		'docx' == file_extension.to_s
+	end
+
+	def pdf?
+		'pdf' == file_extension.to_s
+	end
+
+	def xls?
+		'xls' == file_extension.to_s
+	end
+
+	def pdf_images
+		images = []
+		pdf = Magick::ImageList.new(document.file.path)
+		tmp_path = Rails.root.join('public', 'tmp')
+		#FileUtils::mkdir_p tmp_path
+		Dir.mkdir(tmp_path) unless File.exists?(tmp_path)
+		pdf.each_with_index { |page_img, i|
+			filename = "#{id}_#{i}_pdf_page.jpg"
+			page_img.write("#{tmp_path}/#{filename}")
+			images << "/tmp/#{filename}"
+		}
+		images
+	end
+
+	def to_docx_html
+		content = ''
+		doc = Docx::Document.open(document.file.path)
+		doc.paragraphs.each do |p|
+			if p.node.xpath('w:r//w:lastRenderedPageBreak').present?
+				content << %q(<div class="page-break"></div>) + p.to_html
+			else
+				content << p.to_html
+			end
+		end
+		content
+	end
+
 	private
 	  def clean_s3
 	    document.remove!

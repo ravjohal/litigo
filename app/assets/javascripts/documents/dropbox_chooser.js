52f5ba26b3c740c3ee78178bde93bc32ca283c9e
Slava <fender707@rambler.ru>
2015-09-23 15:55:49 +0300

    [#103959208] - Moved Documents javascript to assets

---
+++ app/assets/javascripts/documents/dropbox_chooser.js
@@ -0,0 +1,39 @@
+options = {
+
+    // Required. Called when a user selects an item in the Chooser.
+    success: function(files) {
+        //alert("Here's the file link: " + files[0].link)
+        $("#db-chooser").attr('href', files[0].link );
+        $("#db-chooser").text(files[0].link);
+    },
+
+    // Optional. Called when the user closes the dialog without selecting a file
+    // and does not include any parameters.
+    cancel: function() {
+
+    },
+
+    // Optional. "preview" (default) is a preview link to the document for sharing,
+    // "direct" is an expiring link to download the contents of the file. For more
+    // information about link types, see Link types below.
+    linkType: "preview", // or "direct"
+
+    // Optional. A value of false (default) limits selection to a single file, while
+    // true enables multiple file selection.
+    multiselect: true, // or true
+
+    // Optional. This is a list of file extensions. If specified, the user will
+    // only be able to select files with these extensions. You may also specify
+    // file types, such as "video" or "images" in the list. For more information,
+    // see File types below. By default, all extensions are allowed.
+    //extensions: ['.pdf', '.doc', '.docx'],
+};
+
+var button = Dropbox.createChooseButton(options);
+document.getElementById("db-container").appendChild(button);
+
+// document.getElementById("db-chooser").addEventListener("DbxChooserSuccess",
+//         function(e) {
+//             // you may have ajax response here. files urls are available there.
+//             $( '#selected-image' ).attr( 'src', e.files[0].link ).fadeIn();
+//          }, false);
---
+++ app/assets/javascripts/documents/form.js
@@ -0,0 +1,1 @@
+$(document).ready(function() { $("#doc_select, td select").select2(); });

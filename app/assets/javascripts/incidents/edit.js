767cc9b8888196c50357febd45cc685c2b80ace3
Slava <fender707@rambler.ru>
2015-09-25 16:26:31 +0300

    [#104074588] - Moved Incidents Javascript to assets

---
+++ app/assets/javascripts/incidents/edit.js
@@ -0,0 +1,6 @@
+$(document).ready(function() { $("select").select2(); });
+$('.tip').tipr({
+    'speed': 500,
+    'mode': 'top'
+});
+
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,4 +2,4 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.assets.precompile += %w( homepage.css .woff .ttf)
-Rails.application.config.assets.precompile += %w( jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js tag-it.js tasks/*.js notes/*.js contacts/*.js documents/*.js time_entries/*.js templates/*.js task_lists/*.js shared/*.js settlements/*.js resolutions/*.js reports/*.js registrations/*.js namespaces/*.js medicals/*.js interrogatories/*.js insurances/*.js injuries/*.js)
+Rails.application.config.assets.precompile += %w( jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js tag-it.js tasks/*.js notes/*.js contacts/*.js documents/*.js time_entries/*.js templates/*.js task_lists/*.js shared/*.js settlements/*.js resolutions/*.js reports/*.js registrations/*.js namespaces/*.js medicals/*.js interrogatories/*.js insurances/*.js injuries/*.js incidents/*.js )

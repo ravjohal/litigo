583fbb45205ae45ebb14eb3b3e0741a18002941a
dimmon908 <dimmon908@gmail.com>
2015-09-28 23:33:28 +0300

    [#103848824] Add logs to schedule jobs classes

--- app/workers/delayed_delete_namespace.rb
+++ app/workers/delayed_delete_namespace.rb
@@ -3,7 +3,9 @@ class DelayedDeleteNamespace
   @queue = :namespaces
 
   def self.perform
+    Rails.logger.fatal "DelayedDeleteNamespace - start - #{Time.now}"
     Namespace.disabled.find_each { |namespace| namespace.destroy }
+    Rails.logger.fatal "DelayedDeleteNamespace - end - #{Time.now}"
   end
 
 end

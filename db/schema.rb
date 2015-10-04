ae4982eca981fbdc8f062f60f9ea3c3001e6a0e0
bensmalley <you@example.com>
2015-10-03 17:21:07 -0700

    Added medical bill end date and styles
    [#104539262]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151001084634) do
+ActiveRecord::Schema.define(version: 20151004001334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -197,6 +197,7 @@ ActiveRecord::Schema.define(version: 20151001084634) do
     t.string   "company_name"
     t.string   "extension"
     t.string   "address_2"
+    t.string   "suffix"
   end
 
   add_index "contacts", ["case_id"], name: "index_contacts_on_case_id", using: :btree
@@ -507,13 +508,28 @@ ActiveRecord::Schema.define(version: 20151001084634) do
   add_index "leads", ["firm_id"], name: "index_leads_on_firm_id", using: :btree
   add_index "leads", ["status"], name: "index_leads_on_status", using: :btree
 
+  create_table "litigations", force: :cascade do |t|
+    t.integer  "firm_id"
+    t.integer  "case_id"
+    t.integer  "parent_id"
+    t.integer  "user_id"
+    t.string   "requester"
+    t.string   "responder"
+    t.text     "interrogatory"
+    t.text     "int_reply"
+    t.date     "int_date"
+    t.date     "reply_date"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
   create_table "medical_bills", force: :cascade do |t|
     t.string   "provider"
     t.date     "date_of_service"
     t.decimal  "billed_amount"
     t.decimal  "paid_amount"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
     t.integer  "company_id"
     t.integer  "medical_id"
     t.integer  "parent_id"
@@ -524,6 +540,7 @@ ActiveRecord::Schema.define(version: 20151001084634) do
     t.decimal  "adjustments"
     t.string   "account_number"
     t.string   "services"
+    t.date     "last_date_of_service"
   end
 
   add_index "medical_bills", ["case_id"], name: "index_medical_bills_on_case_id", using: :btree

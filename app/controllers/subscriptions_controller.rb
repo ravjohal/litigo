fdd39c4b0ebc0e61a9236ecc2c08cb02181a1575
Slava <fender707@rambler.ru>
2015-10-12 00:43:49 +0300

    [#86566064, #91665754] - Added easy change plan just click sign_up on different plan

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,7 +2,7 @@ class SubscriptionsController < ApplicationController
 
   before_filter :authenticate_user!
   before_action :set_user, :set_firm
-  before_action :set_subscription, only: [:show, :edit, :update_plan, :destroy]
+  before_action :set_subscription, only: [:show, :edit, :update_plan, :change_plan,  :destroy]
   def new
     plan = Plan.find(params[:plan_id])
     @subscription = plan.subscriptions.build
@@ -11,10 +11,18 @@ class SubscriptionsController < ApplicationController
   def create
     user = current_user
     @subscription = Subscription.new(subscription_params)
-    price = @subscription.plan.price.to_i
     count_people = @firm.users.count.to_i
-    if @subscription.save_with_payment(price, count_people, user )
+    if @subscription.save_with_payment(count_people, user )
       redirect_to plans_path, :notice => "Thank you for subscribing!"
+    else
+      render :new
+    end
+  end
+
+  def change_plan
+    plan = Plan.find(params[:plan_id])
+    if @subscription.change_subscription(plan)
+      redirect_to plans_path, :notice => "Plan succesfuly changed!"
     else
       render :new
     end

a0ec575ab615e8769fd5e8a36a29241c18f4bdf1
Slava <fender707@rambler.ru>
2015-10-13 18:58:03 +0300

    [#91665754,#86566064] - Added details to subscription, added access to change card with stripe

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,7 +2,7 @@ class SubscriptionsController < ApplicationController
 
   before_filter :authenticate_user!
   before_action :set_user, :set_firm
-  before_action :set_subscription, only: [:show, :edit, :update_plan, :change_plan,  :destroy]
+  before_action :set_subscription, only: [:show, :edit, :update_plan,:edit_card, :update, :change_plan,  :destroy]
   def new
     plan = Plan.find(params[:plan_id])
     @subscription = plan.subscriptions.build
@@ -29,10 +29,23 @@ class SubscriptionsController < ApplicationController
     end
   end
 
+  def edit_card
+  end
+
+  def update
+    if @subscription.update(subscription_params)
+      if @subscription.update_card(@subscription)
+        redirect_to @subscription,  :notice => "Saved. Your card information has been updated."
+      else
+        redirect_to @subscription, :notice => "Stripe reported an error while updating your card. Please try again."
+      end
+    end
+  end
+
   def destroy
     plan = @subscription.plan_id
     if @subscription.cancel_plan(plan)
-      redirect_to plans_path, :notice => "Thank you for subscribing!"
+      redirect_to plans_path, :notice => "Your subscription has been canceled"
     else
       render :new
     end
@@ -46,6 +59,6 @@ private
     @subscription = Subscription.find(params[:id])
   end
   def subscription_params
-    params.require(:subscription).permit(:plan_id, :user_id, :email, :stripe_customer_token,:stripe_card_token, :created_at, :updated_at )
+    params.require(:subscription).permit(:plan_id, :user_id, :email, :last_digits, :stripe_customer_token,:stripe_card_token, :created_at, :updated_at )
   end
 end

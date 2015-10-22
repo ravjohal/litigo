5bccba95aae269c4f7e72b8b6779920dfba944c3
dimmon908 <dimmon908@gmail.com>
2015-10-22 16:38:50 +0300

    [#105974500] Created Payment model, payments tab on invoice page --skip-ci

---
+++ app/assets/javascripts/invoices/show.js
@@ -0,0 +1,5 @@
+$(function() {
+  $('#print').tipsy({gravity: 'n', fade: true});
+  $('#delete').tipsy({gravity: 'n', fade: true});
+  $('#add_payment').find('span').tipsy({gravity: 'n', fade: true});
+});
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,5 +2,5 @@
 
 # Configure sensitive parameters which will be filtered from the log file.
 Rails.application.config.assets.precompile += %w( homepage.css .woff .ttf)
-Rails.application.config.assets.precompile += %w( jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js tag-it.js tasks/*.js notes/*.js contacts/*.js documents/*.js time_entries/*.js templates/*.js task_lists/*.js shared/*.js settlements/*.js resolutions/*.js reports/*.js registrations/*.js namespaces/*.js medicals/*.js interrogatories/*.js insurances/*.js injuries/*.js incidents/*.js expenses/*.js client_intakes/*.js cases/*.js notifications/*.js dashboards/*.js invoices/form.js)
+Rails.application.config.assets.precompile += %w( jasny-bootstrap.js jquery.collapsible.js jquery.feedback_me.js tipr.js sugar.min.js linq.min.js tag-it.js tasks/*.js notes/*.js contacts/*.js documents/*.js time_entries/*.js templates/*.js task_lists/*.js shared/*.js settlements/*.js resolutions/*.js reports/*.js registrations/*.js namespaces/*.js medicals/*.js interrogatories/*.js insurances/*.js injuries/*.js incidents/*.js expenses/*.js client_intakes/*.js cases/*.js notifications/*.js dashboards/*.js invoices/*.js)
 Rails.application.config.assets.precompile += %w( analytics/analytic_application.css analytics/analytic_application.js )

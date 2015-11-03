33c7c8a2f33410358d66c504327fc8320b0cd15c
ravjohal <ravneetjohal@gmail.com>
2015-11-02 23:07:09 -0800

    subscription fix --skip-ci

--- app/mailers/striper.rb
+++ app/mailers/striper.rb
@@ -2,16 +2,22 @@ class Striper < ActionMailer::Base
 
   def change_subscription(subscription)
     @subscription = subscription
-    mail(:to => subscription.email, :subject => "New Subscription Plan", :from => "litigoinsights@gmail.com")
+    if subscription
+      mail(:to => subscription.email, :subject => "New Subscription Plan", :from => "litigoinsights@gmail.com")
+    end
   end
 
   def invoice_payment_failed(subscription)
     @subscription = subscription
-    mail(:to => subscription.email, :subject => "Payment failed", :from => "litigoinsights@gmail.com")
+    if subscription  
+      mail(:to => subscription.email, :subject => "Payment failed", :from => "litigoinsights@gmail.com")
+    end
   end
 
   def invoice_succeed(subscription)
     @subscription = subscription
-    mail(:to => subscription.email, :subject => "Payment succeeded", :from => "litigoinsights@gmail.com")
+    if subscription
+      mail(:to => subscription.email, :subject => "Payment succeeded", :from => "litigoinsights@gmail.com")
+    end
   end
 end

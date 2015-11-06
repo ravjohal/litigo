aa0aba2ea809243a44fa3b194bffb1a5911da5e3
dimmon908 <dimmon908@gmail.com>
2015-11-06 12:35:49 +0200

    [#107369376] Change plans index page for iframe--skip-ci

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -7,6 +7,8 @@ class PlansController < ApplicationController
     @own_subscription = current_user.subscriptions.last
     @plans = Plan.where('id = ? or id = ? or id = ?', 1, 4, 5).order('price')   # for now we need only basic plans(1,4 -ids) and 5 - free plan
     @user_id = params[:user_id]
+
+    render layout: 'only_content' if params[:only_content]
   end
 
   def plan_params

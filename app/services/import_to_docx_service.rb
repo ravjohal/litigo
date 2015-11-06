45fc09318bd67efac0acec72c177a91b510ea8b9
dimmon908 <dimmon908@gmail.com>
2015-11-06 13:21:12 +0200

    [#107157370] Change invoice docx import and add recipient information to it--skip-ci

---
+++ app/services/import_to_docx_service.rb
@@ -0,0 +1,133 @@
+class ImportToDocxService
+
+  def initialize(name, html)
+    @name = name
+    @html = html
+  end
+
+  def generate_docx
+    tmp_file = Htmltoword::Document.create_with_content(Htmltoword::Document.default_xslt_template, @name, @html)
+
+    html_doc = Nokogiri::HTML(@html)
+    html_root = html_doc.css 'body'
+
+    tmp_file_path = tmp_file.path
+
+    new_tmp_file = Tempfile.new(@name)
+    new_tmp_file_name = new_tmp_file.path
+
+    html_root_childs = html_root.children.css('p, hr')
+
+    doc = Docx::Document.open(tmp_file_path)
+    doc.paragraphs.each_with_index do |p, index|
+
+      break unless html_root_childs[index]
+      html_row_class = html_root_childs[index].attr('class').to_s
+
+      if html_row_class =~ /columns/
+
+        if html_row_class =~ /two_columns/
+          wppr = Nokogiri::XML::Node.new('w:pPr', doc.doc)
+          w_tabs = Nokogiri::XML::Node.new('w:tabs', doc.doc)
+          w_tab1 = Nokogiri::XML::Node.new('w:tab', doc.doc)
+
+          w_tab1['w:val']     = 'right'
+          w_tab1['w:pos']     = '8700'
+          w_tab1['w:leader']  = 'none'
+
+          w_tabs.add_child w_tab1
+          wppr.add_child w_tabs
+
+          p.node.children.first.add_previous_sibling wppr if p.node.children.first
+
+          if p.node.xpath('w:r').count > 1
+            _tmp_second_row = p.node.xpath('w:r')[1].xpath('w:t')[0]
+
+            w_tab_tmp = Nokogiri::XML::Node.new('w:tab', doc.doc)
+
+            _row = p.node.xpath('w:r')[0]
+            _row.add_child w_tab_tmp
+            _row.add_child _tmp_second_row
+
+            p.node.xpath('w:r')[1].remove
+          end
+
+        elsif html_row_class =~ /tab_left_column/
+          wppr = Nokogiri::XML::Node.new('w:pPr', doc.doc)
+          w_tabs = Nokogiri::XML::Node.new('w:tabs', doc.doc)
+          w_tab1 = Nokogiri::XML::Node.new('w:tab', doc.doc)
+
+          w_tab1['w:val']     = 'left'
+          w_tab1['w:pos']     = '1000'
+          w_tab1['w:leader']  = 'none'
+
+          w_tabs.add_child w_tab1
+          wppr.add_child w_tabs
+
+          if p.node.children.count > 0
+            p.node.children.first.add_previous_sibling wppr
+
+            w_tab_tmp = Nokogiri::XML::Node.new('w:tab', doc.doc)
+
+            _row = p.node.xpath('w:r')[0]
+            _row.children.first.add_previous_sibling w_tab_tmp
+          end
+
+        elsif html_row_class =~ /two_left_columns/
+          wppr = Nokogiri::XML::Node.new('w:pPr', doc.doc)
+          w_tabs = Nokogiri::XML::Node.new('w:tabs', doc.doc)
+          w_tab1 = Nokogiri::XML::Node.new('w:tab', doc.doc)
+
+          w_tab1['w:val']     = 'left'
+          w_tab1['w:pos']     = '5100'
+          w_tab1['w:leader']  = 'none'
+
+          w_tabs.add_child w_tab1
+          wppr.add_child w_tabs
+
+          p.node.children.first.add_previous_sibling wppr
+
+          if html_row_class =~ /double_columns/
+            if p.node.xpath('w:r').count > 1
+              _tmp_second_row = p.node.xpath('w:r')[1].xpath('w:t')[0]
+
+              w_tab_tmp = Nokogiri::XML::Node.new('w:tab', doc.doc)
+
+              _row = p.node.xpath('w:r')[0]
+              _row.add_child _tmp_second_row
+              _row.add_child w_tab_tmp
+              p.node.xpath('w:r')[1].remove
+
+              _tmp_second_row = p.node.xpath('w:r')[1].xpath('w:t')[0]
+              _row.add_child _tmp_second_row
+              p.node.xpath('w:r')[1].remove
+
+              _tmp_second_row = p.node.xpath('w:r')[1].xpath('w:t')[0]
+              _row.add_child _tmp_second_row
+              p.node.xpath('w:r')[1].remove
+            end
+          else
+            if p.node.xpath('w:r').count > 1
+              _tmp_second_row = p.node.xpath('w:r')[1].xpath('w:t')[0]
+
+              w_tab_tmp = Nokogiri::XML::Node.new('w:tab', doc.doc)
+
+              _row = p.node.xpath('w:r')[0]
+              _row.add_child w_tab_tmp
+              _row.add_child _tmp_second_row
+
+              p.node.xpath('w:r')[1].remove
+            end
+          end
+
+        end
+
+      end
+    end
+
+    doc.save new_tmp_file_name
+
+    new_tmp_file_name
+  end
+
+end

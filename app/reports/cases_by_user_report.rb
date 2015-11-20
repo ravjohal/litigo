97a91b1829b55d5064e1155827bd2cb60abf768a
ravjohal <ravneetjohal@gmail.com>
2015-11-20 02:08:17 -0800

    [deliver #108105846] - cases by user report adding status --skip-ci

--- app/reports/cases_by_user_report.rb
+++ app/reports/cases_by_user_report.rb
@@ -3,7 +3,8 @@ class CasesByUserReport < Dossier::Report
   def sql
     Case.joins(:case_contacts => :contact).where("cases.firm_id = :firm_id 
       AND role IN ('Attorney', 'Staff')
-      AND user_account_id IN :user_contact_id"
+      AND user_account_id IN :user_contact_id
+      AND cases.status IN :status"
       ).select("last_name, first_name, case_number, name, subtype, status"
       ).uniq.to_sql
   end
@@ -16,4 +17,7 @@ class CasesByUserReport < Dossier::Report
     User.find(options[:user_contact_id])
   end
 
+  def status
+    options[:status]
+  end
 end

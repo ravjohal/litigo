167416c79f210992aeb990f969fedf747430c7ee
ravjohal <ravneetjohal@gmail.com>
2015-12-15 00:31:47 -0800

    [deliver #109560444] --skip-ci

---
+++ db/migrate/20151215083054_add_emails_to_cache_migration.rb
@@ -0,0 +1,11 @@
+class AddEmailsToCacheMigration < ActiveRecord::Migration
+
+  def up
+    Participant.find_each { |user| user.load_into_soulmate }
+    Contact.find_each     { |user| user.load_into_soulmate }
+  end
+
+  def down
+
+  end
+end

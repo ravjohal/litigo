ccea3799dcf3483623ea9dfddc686245810738c8
ravjohal <ravneetjohal@gmail.com>
2016-03-19 01:29:22 -0700

    [#115223555] --skip-ci

--- app/reports/count_of_cases_by_status_report.rb
+++ app/reports/count_of_cases_by_status_report.rb
@@ -1,7 +1,22 @@
 class CountOfCasesByStatusReport < Dossier::Report
 
   def sql
-    Case.where("firm_id = :firm_id").select("status, count(status)").group("status").to_sql
+    # OLD: Case.where("firm_id = :firm_id").select("status, count(status)").group("status").to_sql
+
+    "SELECT status, count(status) FROM cases JOIN (
+    	VALUES 
+    	('Active', 1), 
+    	('Treating', 2), 
+    	('Done Treating', 3), 
+    	('Finals Requested', 4),
+    	('Settlement Package Out', 5),
+    	('Negotiation', 6),
+    	('Litigation', 7),
+    	('Pending Close', 8),
+    	('Closed', 9),
+    	('Appeal', 10),
+    	('Inactive', 11)
+    	) as x (st, ordering) on cases.status = x.st WHERE firm_id = :firm_id GROUP BY x.ordering, status ORDER BY x.ordering"
   end
 
   def firm_id

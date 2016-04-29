44360b3d74f0cfbfc509fcf95492368e68e9147c
ravjohal <ravneetjohal@gmail.com>
2016-04-29 00:51:44 -0700

    [#118268189] --skip-ci

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -80,8 +80,8 @@ class ReportsController < ApplicationController
     all_statuses.delete("Closed")
     all_statuses.delete("Inactive")
     open_statuses = all_statuses
-    @open_count = Case.where(:status => open_statuses).count
-    @closed_count = Case.where(:status =>  ['Inactive', 'Closed']).count
+    @open_count = Case.where(:firm_id => @firm.id, :status => open_statuses).count
+    @closed_count = Case.where(:firm_id => @firm.id, :status =>  ['Inactive', 'Closed']).count
     
     @count_of_cases_by_status_report = CountOfCasesByStatusReport.new(firm_id: @firm.id)
   end  

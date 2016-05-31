bfa66b0ebf4c0c2cbfd495e52fd33de05c4eb27a
ravjohal <ravneetjohal@gmail.com>
2016-05-30 22:52:26 -0700

    debugging --skip-ci

--- app/models/concerns/sync_namespaces.rb
+++ app/models/concerns/sync_namespaces.rb
@@ -15,7 +15,7 @@ module SyncNamespaces
 
           last_cursor = nil
 
-          ns.deltas(cursor, Namespace::NYLAS_EXCLUDE_DELTA) do |n_event, ne| #grab all the deltas as of the cursor (new and modified)
+          ns.deltas(cursor) do |n_event, ne| #grab all the deltas as of the cursor (new and modified)
 
             puts "n_event n_event n_event n_event n_event = " + n_event.to_s
 

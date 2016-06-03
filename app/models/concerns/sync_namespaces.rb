87d46dc145996bf62f36a6de082b888e56e697e8
ravjohal <ravneetjohal@gmail.com>
2016-06-02 22:40:54 -0700

    debugging calendar --skip-ci

--- app/models/concerns/sync_namespaces.rb
+++ app/models/concerns/sync_namespaces.rb
@@ -38,66 +38,66 @@ module SyncNamespaces
 
 
 
+        #DELTA CODE
+        active_calendars = namespace.active_calendars #grab only calendars that each namespace (per user) has active
+        if active_calendars.present? #if there is at least one active calendar
+          ns = namespace.nylas_inbox #create a new Nylas Inbox object
 
-        # active_calendars = namespace.active_calendars #grab only calendars that each namespace (per user) has active
-        # if active_calendars.present? #if there is at least one active calendar
-        #   ns = namespace.nylas_inbox #create a new Nylas Inbox object
+          # puts " ns ns ns ns ns ns ns ns ns ns = " + ns.to_s
 
-        #   # puts " ns ns ns ns ns ns ns ns ns ns = " + ns.to_s
+          cursor = namespace.nylas_cursor #figure out what the last cursor was (where the last sync left off)
 
-        #   cursor = namespace.nylas_cursor #figure out what the last cursor was (where the last sync left off)
+          last_cursor_from_nylas = ns.latest_cursor
 
-        #   last_cursor_from_nylas = ns.latest_cursor
+          puts "DO THE CURSORS MATCHUP??????  ===============> from DB: " + cursor.to_s + " from nylas: " + last_cursor_from_nylas.to_s
 
-        #   puts "DO THE CURSORS MATCHUP??????  ===============> from DB: " + cursor.to_s + " from nylas: " + last_cursor_from_nylas.to_s
+          last_cursor = nil
 
-        #   last_cursor = nil
+          ns.deltas(last_cursor_from_nylas) do |n_event, ne| #grab all the deltas as of the cursor (new and modified)
 
-        #   ns.deltas(last_cursor_from_nylas) do |n_event, ne| #grab all the deltas as of the cursor (new and modified)
+            puts "n_event n_event n_event n_event n_event = " + n_event.to_s
 
-        #     puts "n_event n_event n_event n_event n_event = " + n_event.to_s
+            puts "ne ne ne ne ne ne ne ne ne ne ne ne ne = " + ne.inspect
 
-        #     puts "ne ne ne ne ne ne ne ne ne ne ne ne ne = " + ne.inspect
+            if ne.is_a?(Nylas::Event) #is this an Nylas Event type class that is coming from Nylas?
+              if n_event == 'create' or n_event == 'modify' #is this a create or modify type of Nylas Event?
+                calendar = active_calendars.find_by(calendar_id: ne.calendar_id) #get the calendar for what this event is for
 
-        #     if ne.is_a?(Nylas::Event) #is this an Nylas Event type class that is coming from Nylas?
-        #       if n_event == 'create' or n_event == 'modify' #is this a create or modify type of Nylas Event?
-        #         calendar = active_calendars.find_by(calendar_id: ne.calendar_id) #get the calendar for what this event is for
+                puts "calendar calendar calendar calendar calendar = " + calendar.inspect
 
-        #         puts "calendar calendar calendar calendar calendar = " + calendar.inspect
+                if calendar.present? #if that calendar is actually present
+                  event = Event.find_or_initialize_by(nylas_event_id: ne.id) #find the event and update the NylasEvent ID or create new Litigo Event object
 
-        #         if calendar.present? #if that calendar is actually present
-        #           event = Event.find_or_initialize_by(nylas_event_id: ne.id) #find the event and update the NylasEvent ID or create new Litigo Event object
+                  puts "event event event event event event event event = " + event.inspect
 
-        #           puts "event event event event event event event event = " + event.inspect
+                  event.new_record? ?
+                      new_events += 1 :
+                      if updated_events_hash[event.id]
+                        updating_events += 1
+                      else
+                        updated_events += 1
+                        updated_events_hash[event.id] = true
+                      end
 
-        #           event.new_record? ?
-        #               new_events += 1 :
-        #               if updated_events_hash[event.id]
-        #                 updating_events += 1
-        #               else
-        #                 updated_events += 1
-        #                 updated_events_hash[event.id] = true
-        #               end
+                  puts "new_events new_events new_events new_events = " + new_events.to_s
 
-        #           puts "new_events new_events new_events new_events = " + new_events.to_s
+                  if ne.status == 'cancelled'
+                    event.destroy
+                  else
+                    event.assign_nylas_while_refresh ne, firm, calendar, namespace
+                  end
+                end
+              end
+              events_synced += 1
+              last_cursor = ne.cursor
 
-        #           if ne.status == 'cancelled'
-        #             event.destroy
-        #           else
-        #             event.assign_nylas_while_refresh ne, firm, calendar, namespace
-        #           end
-        #         end
-        #       end
-        #       events_synced += 1
-        #       last_cursor = ne.cursor
+              puts "events_synced events_synced events_synced = " + events_synced.to_s
 
-        #       puts "events_synced events_synced events_synced = " + events_synced.to_s
+            end
+          end
 
-        #     end
-        #   end
-
-        #   namespace.update(cursor: last_cursor) if last_cursor.present?
-        # end
+          namespace.update(cursor: last_cursor) if last_cursor.present?
+        end
       rescue Exception => e
         Rails.logger.fatal "Error while refresh events\n - Time: #{Time.now}\n  - Namespace: #{namespace.email_address}\n Message:#{e.message}\n - Backtrace: #{e.backtrace.join("\n")}"
         errors_count += 1

3c0f12940b42bf79a1a8ac74110f5c3cc7e37154
ravjohal <ravneetjohal@gmail.com>
2016-06-02 23:32:00 -0700

    debugging --skip-ci

--- app/models/namespace.rb
+++ app/models/namespace.rb
@@ -36,7 +36,7 @@ class Namespace < ActiveRecord::Base
   # @return [Integer] the last update of the namespace (to figure out the deltas)
   def nylas_cursor
     # cursor.blank? ? nylas_inbox.latest_cursor(last_sync.to_i) : cursor
-    cursor.blank? ? nylas_inbox.latest_cursor : cursor
+    cursor.blank? ? nylas_inbox.latest_cursor() : cursor
   end
 
   def check_for_exists_in_nylas

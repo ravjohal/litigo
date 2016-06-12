91844442e9ac7ae543df65c08dfb026a7196d7b9
ravjohal <ravneetjohal@gmail.com>
2016-06-12 15:14:22 -0700

    add angela roberts as staff on joy's cases --skip-ci

---
+++ db/migrate/20160612204919_transfer_joy_cases_to_andrea.rb
@@ -0,0 +1,47 @@
+class TransferJoyCasesToAndrea < ActiveRecord::Migration
+  def up
+  	user_source = User.find_by_email("jmiller@tsmslaw.com")
+  	user_target = User.find_by_email("aroberts@tsmslaw.com")
+
+  	contact_source = Contact.find_by_email("jmiller@tsmslaw.com")
+  	contact_target = Contact.find_by_email("aroberts@tsmslaw.com")
+
+ #  	if user_source
+	#   	user_source.cases.each do |case_|
+	#   		case_.user = user_target
+	#   		case_.save!
+	#   	end
+	# end
+
+	if contact_source
+		contact_source.case_contacts.each do |cc|
+			cc_new = CaseContact.new
+			cc_new.affair = cc.affair
+			cc_new.role = cc.role
+			cc_new.firm = cc.firm
+			cc_new.note = cc.note
+			cc_new.contact = contact_target
+			cc_new.save!
+		end
+	end
+  end
+
+  def down
+  	user_source = User.find_by_email("aroberts@tsmslaw.com")
+  	user_target = User.find_by_email("jmiller@tsmslaw.com")
+
+  	contact_source = Contact.find_by_email("aroberts@tsmslaw.com")
+  	contact_target = Contact.find_by_email("jmiller@tsmslaw.com")
+  	
+ #  	if user_source
+	#   	user_source.cases.each do |case_|
+	#   		case_.user = user_target
+	#   		case_.save!
+	#   	end
+	# end
+
+	if contact_source
+		contact_source.case_contacts.delete_all
+	end
+  end
+end

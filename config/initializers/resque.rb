07df40c7f21f76e6ad08bbf56a8449af2198aa4a
ravjohal <ravneetjohal@gmail.com>
2018-03-04 18:53:26 -0800

    getting rid of resque

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,12 +1,12 @@
-if Rails.env.staging? || Rails.env.production?
-  uri = URI.parse ENV['REDISTOGO_URL']
-  Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
-end
+# if Rails.env.staging? || Rails.env.production?
+#   uri = URI.parse ENV['REDISTOGO_URL']
+#   Resque.redis = Redis.new :host => uri.host, :port => uri.port, :password => uri.password
+# end
 
-#Dir["/app/app/workers/*.rb"].each { |file| require file }
+# #Dir["/app/app/workers/*.rb"].each { |file| require file }
 
-require 'resque/scheduler'
-Resque.schedule = YAML.load_file(Rails.root.join('config/rescue_schedule.yml'))
+# require 'resque/scheduler'
+# Resque.schedule = YAML.load_file(Rails.root.join('config/rescue_schedule.yml'))
 
-require 'resque-scheduler'
-require 'resque/scheduler/server'
+# require 'resque-scheduler'
+# require 'resque/scheduler/server'
